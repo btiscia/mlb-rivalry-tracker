f5e594731c57964aae8ceae1d24b105b000a75c1
Vincent Bonaddio <vbonaddio@massmutual.com>
2018-07-25 13:59:49 -0400

    Changed Subquery from DATE_DIM_VW to a JOIN to count holidays

--- TABLEAU/EXTRACTS/PSC/PSC Daily Transactions.sql
+++ TABLEAU/EXTRACTS/PSC/PSC Daily Transactions.sql
@@ -1,15 +1,14 @@
-SELECT TransactionTypeName As "Transaction Type"
-,SourceActivityID As "ActivityID"
+SELECT TransactionTypeName AS "Transaction Type"
+,SourceActivityID AS "ActivityID"
 ,T1.SourceTransactionID AS "Source Transaction ID"
 ,HoldingKey AS "Policy Number"
 ,ItemCount AS "Transaction Count"
 ,CASE 
-     WHEN TRANSACTIONTYPEID = 1 THEN  cast(ReceivedDate as TIMESTAMP(6))
-     WHEN TRANSACTIONTYPEID = 2 THEN cast(LoadDate as TIMESTAMP(6))
+     WHEN TRANSACTIONTYPEID = 1 THEN  Cast(ReceivedDate AS TIMESTAMP(6))
+     WHEN TRANSACTIONTYPEID = 2 THEN Cast(LoadDate AS TIMESTAMP(6))
      WHEN TRANSACTIONTYPEID = 3 THEN LongCompletedDate
 END AS "Date"    
 ,LoggedDate AS "Logged Date"
-,TransDate AS "Transaction Date"
 ,Coalesce(EmployeeLastName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"    
 ,Coalesce(ManagerlastName || ', ' || ManagerFirstName, 'Unknown') AS "Manager"
 ,EmployeeRoleName AS "Employee Role Name"
@@ -25,9 +24,7 @@ END AS "Date"
 ,SiteName AS "Site Name"
 ,WorkEventNumber    AS "Work Event Number"
 ,T1.ExpectedCompletedDate AS "Expected Completed Date"
-,(SELECT Count(*) FROM PROD_DMA_VW.DATE_DIM_VW 
-    WHERE IsHoliday = 1 AND ShortDate BETWEEN Cast("Date" AS DATE) 
-    AND Cast(T1.ExpectedCompletedDate AS DATE)) AS "Holidays"    
+, Count(IsHoliday) AS "Holidays"
 ,TAT
 ,DaysPastTAT AS "Days Past TAT"
 ,MetExpectedIndicator AS "Met Expected Indicator"
@@ -44,5 +41,7 @@ END AS "Completed Flag"
 ,ShortComment AS "Comments"
 ,T1.TransDate AS "Trans Date"
 FROM PROD_DMA_VW.PSC_MART_CURR_IVW T1 
+LEFT JOIN PROD_DMA_VW.DATE_DIM_VW T2 ON T2.ShortDate BETWEEN "Date" AND T1.ExpectedCompletedDate AND IsHoliday = 1
 WHERE (WorkEventDepartmentID = 4 OR DepartmentID = 4)
 AND "Date" >= Add_Months(Current_Date, -3)
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,25,26,27,28,29,30,31,32,33,34,35,36
