dfe5b3f75c72728c1db339f99616a24b9ec4fcfd
William Trombley <wtrombley@massmutual.com>
2020-08-13 12:53:58 -0400

    Added Comments to top. Shortened a number of field names. Added code for NBSubmitDateisHoliday.

--- TABLEAU/EXTRACTS/DEV/ANB/ANB Contract Level Extract.sql
+++ TABLEAU/EXTRACTS/DEV/ANB/ANB Contract Level Extract.sql
@@ -1,14 +1,17 @@
+--ANB Modern Policy Details
+--Contract Level
+
 SELECT '10' as "CalDayssuitCmpltToNBRcvd"
 
-, (SELECT PreviousBusinessDay FROM PROD_DMA_VW.DATE_DIM_VW WHERE ShortDate = CAST(Current_Date AS DATE)) AS PreviousBusinessDayOfToday
+, (SELECT PreviousBusinessDay FROM PROD_DMA_VW.DATE_DIM_VW WHERE ShortDate = CAST(Current_Date AS DATE)) AS PrevBusDayOfToday
 , Distributor
 , Channel
 , ChannelType
 , Product
 , Advisor
 , Null as "Region"
-, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE IssueDate = ShortDate) AS "Issue Date Is Holiday"
-, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE ApplicationSubmitDate = ShortDate) AS "Submit Date Is Holiday"
+, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE IssueDate = ShortDate) AS "IssueDateIsHoliday"
+, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE ApplicationSubmitDate = ShortDate) AS "AppSubDateIsHoliday"
 , AgreementID
 , ProductCode
 , HoldingKey
@@ -29,6 +32,7 @@ SELECT '10' as "CalDayssuitCmpltToNBRcvd"
 , SuitabilitySubmitDate
 , ApplicationSignDate
 , NewBusinessSubmitDate
+,(SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE NewBusinessSubmitDate = ShortDate) AS "NBSubmitDateIsHoliday"
 , IssueDate
 , RejectDate
 , WithdrawnDate
@@ -41,24 +45,24 @@ SELECT '10' as "CalDayssuitCmpltToNBRcvd"
 , BINGODate
 , BINGOStatus
 , BINGOIndicator
-, CASE WHEN IssueDate >= '2018-08-01' THEN 1 ELSE 0 END AS "Issue Count for BINGO Rate"
-, CAST(Current_Date AS DATE) - ApplicationSubmitDate AS CalDaysSinceSubmit
-, CAST(Current_Date AS DATE) - NewBusinessEndDate AS CalDaysSinceNBSubmit
+, CASE WHEN IssueDate >= '2018-08-01' THEN 1 ELSE 0 END AS "IssueCountforBINGORate"
+, CAST(Current_Date AS DATE) - ApplicationSubmitDate AS CalDaysSinceSub
+, CAST(Current_Date AS DATE) - NewBusinessEndDate AS CalDaysSinceNBSub
 , IssueDate - ApplicationSignDate AS CalDaysSignToIssue
-, ApplicationSignDate - ApplicationSubmitDate AS CalDaysSignToSubmit
-, OriginalOrderSubmitDate - ApplicationSignDate AS CalDaysAppSignToSuitSubmit
-, NewBusinessEndDate - ApplicationSignDate AS CalDaysSignToNBSubmit
-, IssueDate - ApplicationSubmitDate AS "Submit to Issue Cycle Time"
-, SuitabilityApprovalDate - ApplicationSubmitDate AS CalDaysSubmitToSuitApvd
-, NewBusinessSubmitDate - ApplicationSubmitDate AS CalDaysSubmtToNBSubmit
+, ApplicationSignDate - ApplicationSubmitDate AS CalDaysSignToSub
+, OriginalOrderSubmitDate - ApplicationSignDate AS CalDaysAppSignToSuitSub
+, NewBusinessEndDate - ApplicationSignDate AS CalDaysSignToNBSub
+, IssueDate - ApplicationSubmitDate AS "SubtoIssueCycleTime"
+, SuitabilityApprovalDate - ApplicationSubmitDate AS CalDaysSubToSuitApvd
+, NewBusinessSubmitDate - ApplicationSubmitDate AS CalDaysSubToNBSub
 , IssueDate - NewBusinessSubmitDate AS CalDaysNBRcvdToIssued
-, BINGODate - NewBusinessSubmitDate AS NBSubmitToBINGO
-, NewBusinessSubmitDate - PAWDate AS CalDaysNBSubmitToPAW
-, SuitabilityApprovalDate - SuitabilitySubmitDate AS CalDaysSuitSubmitToSuitApvd
-, NewBusinessSubmitDate - SuitabilityApprovalDate AS CalDaysSuitApvdToNBSubmit
-, CAST(OrderChangeDate AS DATE) - SuitabilityApprovalDate AS CalDaysSuitApvdToSuitTransmit
-, CAST(OrderChangeDate AS DATE) - SuitabilitySubmitDate AS CalDaysSuitSubmitToSuitTransmit
-, IssueDate - SuitabilitySubmitDate AS CalDaysSuitSubmitToIssue
+, BINGODate - NewBusinessSubmitDate AS NBSubToBINGO
+, NewBusinessSubmitDate - PAWDate AS CalDaysNBSubToPAW
+, SuitabilityApprovalDate - SuitabilitySubmitDate AS CalDaysSuitSubToSuitApvd
+, NewBusinessSubmitDate - SuitabilityApprovalDate AS CalDaysSuitApvdToNBSub
+, CAST(OrderChangeDate AS DATE) - SuitabilityApprovalDate AS CalDaysSuitApvdToSuitTrans
+, CAST(OrderChangeDate AS DATE) - SuitabilitySubmitDate AS CalDaysSuitSubToSuitTrans
+, IssueDate - SuitabilitySubmitDate AS CalDaysSuitSubToIssue
 , PAWDate - BINGODate AS CalDaysBINGOToPAW
 , TOADate - BINGODate AS CalDaysBINGOToTOA
 , IssueDate - TOADate AS CalDaysTOAToIssue
