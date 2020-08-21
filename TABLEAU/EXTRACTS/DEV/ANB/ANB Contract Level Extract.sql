2af23e19cf44a2d62a0d5179b1aeb91f0e99f2c1
MASSMUTUAL\mm33437 <jasonjohnson@massmutual.com>
2020-08-21 13:44:00 -0400

    re ordered columns

--- TABLEAU/EXTRACTS/DEV/ANB/ANB Contract Level Extract.sql
+++ TABLEAU/EXTRACTS/DEV/ANB/ANB Contract Level Extract.sql
@@ -3,49 +3,49 @@
 
 SELECT '10' as "CalDayssuitCmpltToNBRcvd"
 
-, (SELECT PreviousBusinessDay FROM PROD_DMA_VW.DATE_DIM_VW WHERE ShortDate = CAST(Current_Date AS DATE)) AS PrevBusDayOfToday
-, Distributor
-, Channel
-, ChannelType
-, Product
-, Advisor
-, Null as "Region"
-, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE IssueDate = ShortDate) AS "IssueDateIsHoliday"
-, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE ApplicationSubmitDate = ShortDate) AS "AppSubDateIsHoliday"
-, AgreementID
-, ProductCode
 , HoldingKey
 , OrderEntryID
-, AutoApprovalIndicator
-, AnticipatedPremium
-, DepositAmount
+, AgreementID
+, PolicyNumber
 , NewBusinessDocType
 , ContractState
 , ResidenceState
+, Null as "Region"
 , AgentID
+, Advisor
 , AgencyNumber
 , FirmName
+, Distributor
+, Channel
+, ChannelType
+, ProductCode
+, Product
 , ProductCategory
-, PolicyNumber
+, AnticipatedPremium
+, DepositAmount
+, BINGOStatus
+, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE IssueDate = ShortDate) AS "IssueDateIsHoliday"
+, (SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE ApplicationSubmitDate = ShortDate) AS "AppSubDateIsHoliday"
+, BINGOIndicator
+, AutoApprovalIndicator
+,(SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE NewBusinessSubmitDate = ShortDate) AS "NBSubmitDateIsHoliday"
+, CASE WHEN IssueDate >= '2018-08-01' THEN 1 ELSE 0 END AS "IssueCountforBINGORate"
+, (SELECT PreviousBusinessDay FROM PROD_DMA_VW.DATE_DIM_VW WHERE ShortDate = CAST(Current_Date AS DATE)) AS PrevBusDayOfToday
+, ApplicationSubmitDate
 , SuitabilityApprovalDate
 , OriginalOrderSubmitDate
 , SuitabilitySubmitDate
 , ApplicationSignDate
+, OrderChangeDate
 , NewBusinessSubmitDate
-,(SELECT IsHoliday FROM PROD_DMA_VW.DATE_DIM_VW WHERE NewBusinessSubmitDate = ShortDate) AS "NBSubmitDateIsHoliday"
+, PAWDate
+, TOADate
+, BINGODate
+, FirstNIGODate
+, NewBusinessEndDate
 , IssueDate
 , RejectDate
 , WithdrawnDate
-, PAWDate
-, TOADate
-, OrderChangeDate
-, ApplicationSubmitDate
-, FirstNIGODate
-, NewBusinessEndDate
-, BINGODate
-, BINGOStatus
-, BINGOIndicator
-, CASE WHEN IssueDate >= '2018-08-01' THEN 1 ELSE 0 END AS "IssueCountforBINGORate"
 , CAST(Current_Date AS DATE) - ApplicationSubmitDate AS CalDaysSinceSub
 , CAST(Current_Date AS DATE) - NewBusinessEndDate AS CalDaysSinceNBSub
 , IssueDate - ApplicationSignDate AS CalDaysSignToIssue
