2f1a0916fc16cde9a902f6ab14d561c55adba5db
Christian <lchristian@massmutual.com>
2021-02-04 15:06:59 -0500

    SQL Changes

--- TABLEAU/EXTRACTS/DEV/ANB/ANB BINGO DASH IR.sql
+++ TABLEAU/EXTRACTS/DEV/ANB/ANB BINGO DASH IR.sql
@@ -1,12 +1,13 @@
 /*
 * This routine pulls  initial reviews for ANB BINGO dashboard.
-*  Based on cases.  Cases are distinct policy numbers.
+*  Based on cases.  Cases are distinct applications IDs.  .
 *  Peer Review & Change Log:
 *  Peer Review Date: 
 *  Source for this routine is  PROD_DMA_VW_ACT_ANO_CURR_INTEGRATED_FCT_VW
 *  Author: Zach Dorval, Lorraine Christian and Bill Trombley
 *  Created: 2/4/2021
 *  Revisions:  
+*  Notes:  IR will not have "doc type" like SE2.  
 ====================================================================== 
 ======================================================================
                 
@@ -14,28 +15,31 @@
 
 SELECT DISTINCT
 ApplicationID
+, CASE WHEN BingoDate IS Null THEN 0 else 1 end as BINGOIndicator
+, DISTRIBUTOR AS Distributor
+, Channel
+, Firm
+---No doc type for IR
+, Product Category
+----NEED NIGO Category
+, NIGOReason  
+
+, Product
 , StateCode
 , IRMarketTypeName
 , SuitabilityIndicator
 , IGOIndicator
 , DISTRIBUTOR
-, Product
-, Product Category
 , T1.AgentID
 , T1.AGENTID
 , T1.AGENCYNUMBER AS FIRMNUM
-, Firm
 , T2.FirmName
 ,  RegionName
-, DISTRIBUTOR
-, Channel
 , NEWBUSINESSSUBMITDATE
 , ISSUEDATE
-, NIGOReason
 , CAST(T1.CreatedAtDateTimestamp AS DATE) NIGODATE
 , CAST(ISSUEDATE AS DATE) BINGODATE
 , (BINGODATE - NIGODATE) NIGOResolution
-, CASE WHEN BingoDate IS Null THEN 0 else 1 end as BINGOIndicator
 FROM	PROD_DMA_VW.ANB_IR_NIGO_REASON_TOKEN_VW T1
 LEFT JOIN PROD_DMA_VW.ANB_APPLICATION_RPT_VW T2 ON T2.INITIALREVIEWID = T1.INITIALREVIEWID
 LEFT JOIN PROD_DMA_VW.FIRM_DIM_VW T3 ON T1.AGENCYNUMBER = T3.ORIGINALFIRMNUMBER
