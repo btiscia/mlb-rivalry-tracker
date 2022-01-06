0d71b4ccead1ed7ffd7c33d7399f421e0f6d465e
William Trombley <wtrombley@massmutual.com>
2022-01-06 12:19:10 -0500

    Restricted Indicator Removed

---
+++ TABLEAU/EXTRACTS/PROD/DIC/DI Claims Current Restricted Pending Inventory.sql
@@ -0,0 +1,73 @@
+--Tableau Data Source: DI Claims - Current Restricted Pending Inventory
+
+/*
+* This routine pulls all DI Claims - CURRENT Pending Inventory items
+*  Peer Review & Change Log:
+*  Peer Review Date: 
+*  Source for this routine is  
+*  Author: John Avgoustakis/Bill Trombley
+*  Created: 
+* Revision: 1/5/2022 - Bill Trombley - Removed Restricted Claim filter.
+======================================================================
+
+                
+======================================================================*/
+
+SELECT
+T1.LoadDate
+,T1.IntegratedActivityID AS "Integrated Activity ID"
+,T1.SourceTransactionID AS "Source Transaction ID"
+,T1.SystemName AS "System Name"
+,T1.HoldingKey AS "Policy Number"
+,T1.BaseClaimNumber AS "Base Claim Number"
+,T1.ShortClaimNumber AS "Claim #"
+,T1.ReceivedDate AS "Received Date"
+,T1.ExpectedCompletedDate  AS "Expected Completed Date"
+,T1.DaysPending  AS "Days Pending"
+,T1.DaysPastTAT AS "Days Past TAT" 
+,T1.TAT
+,T1.TATGoal AS "TAT Goal"
+,COALESCE(T1.DemandCredit, T1.ProductivityCredit) AS "Productivity Credits"
+,COALESCE(T1.EmployeeRoleName , 'Unknown') AS "Role"
+,COALESCE(T1.EmployeeLastName || ', ' || T1.EmployeeFirstName, 'Unknown') AS "Employee Name"	
+,COALESCE(T1.EmployeeManagerlastName || ', ' || T1.EmployeeManagerFirstName, 'Unknown') AS "Manager Name"
+,COALESCE(T1.EmployeeTeamName, 'Unknown') AS "Team Name"
+,COALESCE(T1.WorkEventFunctionName, 'Unknown') AS "Function Name"
+,COALESCE(T1.WorkEventSegmentName, 'Unknown') AS "Segment Name"
+,COALESCE(T1.WorkEventWorkEventName, 'Unknown') AS "Work Event Name"
+,COALESCE(T1.EmployeeOrganizationName, 'Unknown') AS "Employee Organization Name"
+,COALESCE(T1.EmployeeDepartmentName, 'Unknown') AS "Employee Department Name"
+,COALESCE(T1.WorkEventOrganizationName, 'Unknown') AS "Work Event Organization Name"
+,COALESCE(T1.WorkEventDepartmentName	, 'Unknown') AS  "Work Event Department Name"
+,COALESCE(T1.WorkEventPrimaryRoleName, 'Unknown') AS "Work Event Primary Role Name"
+,COALESCE(T1.DIBSClaimantName, 'Unknown') AS "Claimaint Name" 
+,COALESCE(T1.LogEmployeeLastName || ', ' || T1.LogEmployeeFirstName, 'Unknown') AS "Logged By"
+,CASE WHEN T1.SystemName = 'CATS'  THEN 'CATS' 
+WHEN	T1.WorkEventSystemDepartmentName = 'Diary'  THEN 'DIBS Diary' 
+WHEN	T1.WorkEventSystemDepartmentName = 'Payment'   THEN 'DIBS Payment' 
+WHEN  T1.WorkEventSystemDepartmentName = 'TREX CONTENT CLOSURE LETTER'   THEN 'TREX Closure Letter' 
+WHEN  T1.WorkEventSystemDepartmentName = 'TREX CONTENT STATUS LETTER'   THEN 'TREX Status Letter' 
+WHEN  T1.WorkEventSystemDepartmentName = 'TREX CONTENT'   THEN 'TREX Content'
+WHEN  T1.WorkEventSystemDepartmentName = 'TREX MAIL'   THEN 'TREX Mail'
+WHEN  T1.WorkEventSystemDepartmentName = 'TREX WORK'   THEN 'TREX Work'
+WHEN T1.SystemName = 'MEDVOC' THEN 'MEDVOC' 
+ELSE 'UNKNOWN' END AS "System"
+,T1.PriorityName AS "Prioirty"
+,T1.ReviewStatus AS "Work Status"
+,T2.AppealIndicator AS "Appeal Indicator"
+,T2.ContestableIndicator AS "Contestable Indicator"
+,T2.ERISAIndicator AS "ERISA Indicator"
+,T2.LateNoticeIndicator AS "Late Notice Indicator"
+,T2.QuickDecisionIndicator AS "Quick Decision Indicator"
+,T1.RestrictedClaimIndicator 
+,COUNT(DISTINCT T1.IntegratedActivityID) AS "Transaction Count"    
+,MAX(T1.TransDate)	AS "Transaction Date"
+
+FROM PROD_DMA_VW.ACT_DIC_CURR_INTEGRATED_VW T1
+INNER JOIN PROD_DMA_VW.DI_CLAIM_CURR_DIM_VW T2 ON T1.CLAIMNUMBER = T2.CLAIMNUMBER
+
+WHERE
+TransactionTypeID = 2
+AND T1.LoadDate = CURRENT_DATE
+
+GROUP BY  1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37
---
+++ TABLEAU/EXTRACTS/PROD/DIC/DI Claims Restricted Payment Risk.sql
@@ -0,0 +1,58 @@
+--Tableau Data Source: DI Claims Restricted Payment Risk
+
+/*
+*  Peer Review & Change Log:
+*  Peer Review Date: 
+*  Source for this routine is  
+*  Author: John Avgoustakis
+*  Created: 12/12/2019
+* Revision: 1/5/2022 - Bill Trombley - Removed Restricted Claim filter.
+======================================================================
+*/
+
+
+
+SELECT DISTINCT 
+T1.ClaimDimensionUniqueID
+,T1.ClaimNumber AS "Claim Number"
+ ,T1.ShortClaimNumber AS "Short Claim Number"
+ --,T1.LastItemID AS PaymentID
+ -- ,T1.LastItemType AS "Base Payment Type"
+,CASE 
+	WHEN T1.LastItemType = 'Payment Transaction - TD' THEN 'Total' 
+	WHEN T1.LastItemType = 'Payment Transaction - PD' THEN 'Partial'
+END AS "Base Payment Type"
+,T1.LoadDate AS "Load Date"
+,T1.ClaimCurrentStatus AS "Claim Current Status"
+,T1.ClaimCurrentSubstatus AS "Claim Current Substatus" 
+,T1.ClaimCategory AS "Claim Category"
+,T1.ClaimStatusCategory AS "Claim Status Category"
+,T1.ClaimantName AS "Claimant Name" 
+,T1.ClaimantAge AS "Claimant Age"
+,T1.MonthsSinceDateofDisability
+,T1.ExaminerName AS "Examiner"
+,T1.ExaminerManagerName AS "Manager"
+,T1.ExaminerTeam AS "Team"
+,T1.MedRvwSupportedThroughDate AS "Supported Through Date"
+,T1.TransDate AS "Trans Date" 
+,T2.LastItemDate AS "Past Due Check Date"
+,COALESCE(NoPaymentIndicator, 0) AS "No Payment Indicator"
+,COALESCE(NoThroughIndicator, 0) AS "No Through Indicator"
+,RestrictedClaimIndicator
+,T3.LastItemDate AS "Past Due Through Date"
+--,CASE WHEN T1.LastItemID IS NULL THEN 'Missing' ELSE 'Past Due' END AS "Status"
+FROM    PROD_DMA_VW.DIC_RISK_INVENTORY_RPT_VW T1
+
+-- Left  Join to No Payment Data
+LEFT JOIN (SELECT LastItemID, LoadDate,ClaimNumber, LastItemDate, RiskTypeID, 1 NoPaymentIndicator
+						FROM PROD_DMA_VW.DIC_RISK_INVENTORY_RPT_VW 
+						WHERE RiskTypeID = 1 
+						AND (CalendarDaysPastTAT > 0 OR CalendarDaysPastTAT IS NULL)) T2 ON T1.ClaimNumber = T2.ClaimNumber AND T1.LoadDate = T2.LoadDate 
+
+-- Left Join to No Through Payment Data
+LEFT JOIN  (SELECT LastItemID, LoadDate, ClaimNumber, LastItemDate, RiskTypeID, 1 NoThroughIndicator
+						FROM PROD_DMA_VW.DIC_RISK_INVENTORY_RPT_VW 
+						WHERE RiskTypeID = 8 
+						AND (CalendarDaysPastTAT > 0 OR CalendarDaysPastTAT IS NULL)) T3 ON T1.ClaimNumber = T3.ClaimNumber AND T1.LoadDate = T3.LoadDate 
+WHERE T1.RiskTypeID IN (1,8)
+AND (CalendarDaysPastTAT > 0 OR CalendarDaysPastTAT IS NULL)
