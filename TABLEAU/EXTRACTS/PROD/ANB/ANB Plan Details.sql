23dbf5597718ccc326144fe2ee8e09cb97f6be0a
William Trombley <wtrombley@massmutual.com>
2022-02-11 09:09:23 -0500

    Update ANB Plan Details.sql
    Changes to account for product

--- TABLEAU/EXTRACTS/PROD/ANB/ANB Plan Details.sql
+++ TABLEAU/EXTRACTS/PROD/ANB/ANB Plan Details.sql
@@ -9,7 +9,7 @@
 --   PROD_DMA_VW.ANB_APPLICATION_RPT_VW
 -- Author: Bill Trombley
 -- Created: 6/28/2021
--- Revised:  
+-- Revised:  2/11/2022
 
 
 SELECT T1.Volumetric AS PlanMetric
@@ -17,7 +17,8 @@ SELECT T1.Volumetric AS PlanMetric
 , T1.IsHoliday AS "IsHoliday"
 , T1.IsWeekday
 , T1.ChannelType AS Channel
-, T1.ProductType AS Product
+, T1.ProductType AS ProductType
+,T1.ProductName AS ProductName
 , Measure
 , DailyKPIPlan
 , DailyMTDPlan
@@ -32,33 +33,34 @@ SELECT T1.Volumetric AS PlanMetric
 		, CASE GROUPING(TCAT.VOLUMETRIC) WHEN 1 THEN 'Total' ELSE TCAT.VOLUMETRIC END AS Volumetric
 		, CASE GROUPING(TCAT.PRODUCTTYPE) WHEN 1 THEN 'Total' ELSE TCAT.PRODUCTTYPE END AS ProductType
 		, CASE GROUPING(TCAT.CHANNELTYPE) WHEN 1 THEN 'Total' ELSE TCAT.CHANNELTYPE END AS ChannelType
+		, CASE GROUPING(TCAT.PRODUCTNAME) WHEN 1 THEN 'Total' ELSE TCAT.PRODUCTNAME END AS ProductName --Added grouping for ProductName
 		, SUM(DAILYKPIPLAN) AS DailyKPIPlan
 		, SUM(DAILYMTDPLAN) AS DailyMTDPlan
 		, SUM(DAILYYTDPLAN) AS DailyYTDPlan
 		FROM PROD_DMA_VW.Date_DIM_VW T2 
 		
-		----------------------------------Begin TCAT------------------------------------
+		----------------------------------Begin TCAT-----------------------------------
 		JOIN (SELECT DISTINCT VOLUMETRIC
 		, PRODUCTTYPE
-		, CHANNELTYPE 
-		FROM PROD_DMA_VW.PRD_KPI_PLAN_DATA_VW
+		, CHANNELTYPE
+		, ProductName
+		
+		FROM DMA_GRP_DL.PRD_KPI_PLAN_DATA_VW
 		WHERE CHANNELTYPE <> 'Total' AND DEPARTMENTID = 47) TCAT ON 1=1
 		----------------------------------End TCAT------------------------------------
 		
-		LEFT JOIN PROD_DMA_VW.PRD_KPI_PLAN_DATA_VW T1 ON T2.ShortDate = T1.ShortDate 
+		LEFT JOIN DMA_GRP_DL.PRD_KPI_PLAN_DATA_VW T1 ON T2.ShortDate = T1.ShortDate 
 			AND  T1.CHANNELTYPE <> 'Total' 
 			AND T1.DEPARTMENTID = 47
 			AND T1.VOLUMETRIC = TCAT.VOLUMETRIC 
 			AND T1.PRODUCTTYPE = TCAT.PRODUCTTYPE 
 			AND T1.CHANNELTYPE = TCAT.CHANNELTYPE
+			AND T1.PRODUCTNAME = TCAT.PRODUCTNAME --Added ProductName
 		
-		WHERE T2.SHORTDATE BETWEEN (ADD_MONTHS(TRUNC(CURRENT_DATE, 'YEAR'),-48))  AND (ADD_MONTHS(TRUNC(CURRENT_DATE, 'YEAR'),12)-1)
+		WHERE T2.SHORTDATE BETWEEN (ADD_MONTHS(TRUNC(CURRENT_DATE, 'YEAR'),-12))  AND (ADD_MONTHS(TRUNC(CURRENT_DATE, 'YEAR'),12)-1) 
+				
+		GROUP BY ROLLUP(TCAT.VOLUMETRIC,TCAT.CHANNELTYPE,TCAT.PRODUCTTYPE,TCAT.PRODUCTNAME,IsHoliday ),1,2,3) T1 --Adjusted rollup to add ProductName
 		
-		----------------------------------Begin use to test change of year------------------------------------
-		--WHERE T2.SHORTDATE BETWEEN  (CURRENT_DATE - INTERVAL '5' YEAR) AND ADD_MONTHS(TRUNC(CAST('2022-01-01' AS DATE), 'YEAR'),12)-1
-		----------------------------------End  use to test change of year------------------------------------
-		
-		GROUP BY ROLLUP(TCAT.VOLUMETRIC,TCAT.CHANNELTYPE,TCAT.PRODUCTTYPE,IsHoliday ),1,2,3) T1
 		----------------------------------End T1------------------------------------
 
 
@@ -70,18 +72,25 @@ SELECT T1.Volumetric AS PlanMetric
 			, CASE GROUPING(CAST(T1.ShortDate AS VARCHAR(10))) WHEN 1 THEN 'Total' ELSE CAST(T1.ShortDate AS VARCHAR(10)) END AS ShortDate
 			, CASE GROUPING(T1.PRODUCTTYPE) WHEN 1 THEN 'Total' ELSE T1.PRODUCTTYPE END AS ProductType
 			, CASE GROUPING(T1.CHANNELTYPE) WHEN 1 THEN 'Total' ELSE T1.CHANNELTYPE END AS ChannelType
+			,CASE GROUPING(T1.PRODUCTNAME) WHEN 1 THEN 'Total' ELSE T1.PRODUCTNAME END AS ProductName  --Added productname
 			, SUM(T1.MEASURE) AS Measure
 			FROM 
 		
 				----------------------------------Begin T1------------------------------------
-				(
-					SELECT 'Submitted WTD Prem' AS Volumetric
+				(	SELECT 'Submitted WTD Prem' AS Volumetric
 					, NewBusinessSubmitDate AS ShortDate
 					, ProductCategory AS ProductType
 					, Channel AS ChannelType
+					,CASE WHEN Product = 'RetireEase SPIA' THEN 'RetireEase'
+ 							   WHEN Product LIKE 'Capital Vantage%' THEN 'Capital Vantage'
+                               WHEN Product LIKE 'Transitions Select%' THEN 'Transitions Select'
+                               WHEN Product LIKE 'RetireEase Choice%' THEN 'RetireEase Choice'
+							   WHEN Product LIKE 'Envision%' THEN 'Envision'
+							   WHEN Product LIKE 'Rila%' THEN 'Rila'
+ 							   ELSE Product END AS ProductName             --This adjusts for the productname differences between the reporting view and KPI table
 					, SUM(AnticipatedPremium) AS Measure
 					FROM PROD_DMA_VW.ANB_APPLICATION_RPT_VW
-					GROUP BY 1,2,3,4
+					GROUP BY 1,2,3,4,5
 					
 					UNION ALL
 					
@@ -89,9 +98,16 @@ SELECT T1.Volumetric AS PlanMetric
 					, IssueDate AS ShortDate
 					, ProductCategory AS ProductType
 					, Channel AS ChannelType
+					,CASE WHEN Product = 'RetireEase SPIA' THEN 'RetireEase'
+ 							   WHEN Product LIKE 'Capital Vantage%' THEN 'Capital Vantage'
+                               WHEN Product LIKE 'Transitions Select%' THEN 'Transitions Select'
+                               WHEN Product LIKE 'RetireEase Choice%' THEN 'RetireEase Choice'
+							   WHEN Product LIKE 'Envision%' THEN 'Envision'
+							   WHEN Product LIKE 'Rila%' THEN 'Rila'
+ 							   ELSE Product END AS ProductName             --This adjusts for the productname differences between the reporting view and KPI table
 					, SUM(DepositAmount) AS Measure
 					FROM PROD_DMA_VW.ANB_APPLICATION_RPT_VW
-					GROUP BY 1,2,3,4
+					GROUP BY 1,2,3,4,5
 					
 					UNION ALL
 					
@@ -99,9 +115,16 @@ SELECT T1.Volumetric AS PlanMetric
 					, NewBusinessSubmitDate AS ShortDate
 					, ProductCategory AS ProductType
 					, Channel AS ChannelType
+					,CASE WHEN Product = 'RetireEase SPIA' THEN 'RetireEase'
+ 							   WHEN Product LIKE 'Capital Vantage%' THEN 'Capital Vantage'
+                               WHEN Product LIKE 'Transitions Select%' THEN 'Transitions Select'
+                               WHEN Product LIKE 'RetireEase Choice%' THEN 'RetireEase Choice'
+							   WHEN Product LIKE 'Envision%' THEN 'Envision'
+							   WHEN Product LIKE 'Rila%' THEN 'Rila'
+ 							   ELSE Product END AS ProductName             --This adjusts for the productname differences between the reporting view and KPI table
 					, CAST (COUNT(NewBusinessSubmitDate) AS DECIMAL(8,2)) AS Measure
 					FROM PROD_DMA_VW.ANB_APPLICATION_RPT_VW
-					GROUP BY 1,2,3,4
+					GROUP BY 1,2,3,4,5
 					
 					UNION ALL
 					
@@ -109,20 +132,34 @@ SELECT T1.Volumetric AS PlanMetric
 					, IssueDate AS ShortDate
 					, ProductCategory AS ProductType
 					, Channel AS ChannelType
+					,CASE WHEN Product = 'RetireEase SPIA' THEN 'RetireEase'
+ 							   WHEN Product LIKE 'Capital Vantage%' THEN 'Capital Vantage'
+                               WHEN Product LIKE 'Transitions Select%' THEN 'Transitions Select'
+                               WHEN Product LIKE 'RetireEase Choice%' THEN 'RetireEase Choice'
+							   WHEN Product LIKE 'Envision%' THEN 'Envision'
+							   WHEN Product LIKE 'Rila%' THEN 'Rila'
+ 							   ELSE Product END AS ProductName             --This adjusts for the productname differences between the reporting view and KPI table
 					,CAST (COUNT(IssueDate) AS DECIMAL(8,2)) AS Measure
 					FROM PROD_DMA_VW.ANB_APPLICATION_RPT_VW
-					GROUP BY 1,2,3,4
+					GROUP BY 1,2,3,4,5
 				) T1
+				
+							
+				
 			----------------------------------End T1------------------------------------
 	
 	
 		WHERE T1.SHORTDATE >= CURRENT_DATE - INTERVAL '5' YEAR
-		AND ProductType IS NOT NULL AND ChannelType IS NOT NULL
+		AND ProductType IS NOT NULL AND ChannelType IS NOT NULL 
 		
-		GROUP BY ROLLUP(T1.VOLUMETRIC,CAST(T1.ShortDate AS VARCHAR(10)),T1.CHANNELTYPE,T1.PRODUCTTYPE)
+		GROUP BY ROLLUP(T1.VOLUMETRIC,CAST(T1.ShortDate AS VARCHAR(10)),T1.CHANNELTYPE,T1.PRODUCTTYPE, T1.PRODUCTNAME)  --Adjusted rollup to add ProductName
 		
 		) T2
 		----------------------------------End T2------------------------------------
 	
 	ON T1.VOLUMETRIC = T2.VOLUMETRIC AND T2.SHORTDATE = CAST(T1.SHORTDATE AS VARCHAR(10)) 
-	AND T1.PRODUCTTYPE = T2.PRODUCTTYPE AND T1.CHANNELTYPE = T2.CHANNELTYPE
+	AND T1.PRODUCTTYPE = T2.PRODUCTTYPE AND T1.CHANNELTYPE = T2.CHANNELTYPE AND T1.PRODUCTNAME = T2.PRODUCTNAME  --Added join on productname
+	
+	WHERE
+		
+	T1.PRODUCTNAME IS NOT NULL AND T1.PRODUCTNAME <> 'Total'  --This pulls only the lines from the KPI table with the ProductName populated so that the aggregations work correctly
