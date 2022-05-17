f5ccf43de71354c7e8ac6d17da2db2bf316560cc
Vincent Bonaddio <vbonaddio@massmutual.com>
2022-05-17 09:46:48 -0400

    Update DI Claims Caseload.sql

--- TABLEAU/EXTRACTS/PROD/DIC/DI Claims Caseload.sql
+++ TABLEAU/EXTRACTS/PROD/DIC/DI Claims Caseload.sql
@@ -19,13 +19,13 @@ SELECT T5.short_dt AS "Load Date"
       END AS "Claim Status"
 	, T1.current_substatus AS "Claim Substatus"
 	, T1.claim_category AS "Claim Category"
-	, CASE WHEN T1.base_claim_ind = 1 THEN 'Claim' 
+	, CASE WHEN T1.base_claim_ind = 1 THEN 'Claim'
 		ELSE 'Additional Policy' END AS 'Claim/Add''l Policy'
 	, T1.dibs_customer_id AS "DIBS Customer ID"
 	, T4.med_review_category "Med Review Category"
 	, T4.med_review_support_dt "MedReviewSupportDate"
-	, T1.examiner_party_employee_id 
-	, COALESCE(T4.med_review_category, 'No Review') AS "Category" 
+	, T1.examiner_party_employee_id
+	, COALESCE(T4.med_review_category, 'No Review') AS "Category"
 	, T1.disability_dt AS "Disability Date"
 	, T1.late_notice_ind AS "LateNoticeIndicator"
 	, T1.erisa_ind AS "ERISAIndicator"
@@ -63,7 +63,7 @@ SELECT T5.short_dt AS "Load Date"
 	, T1.row_process_dtm AS "TransDate"
     , COALESCE(CASE WHEN T1.claim_status_category = 'Preclaim' THEN T5.short_dt - T1.notice_dt
                 WHEN T1.claim_status_category = 'Active Pending' THEN T5.short_dt - CAST(COALESCE(T2.ro_subst_dt,T2.pc_del_dt,T2.last_pe_subst_dt) AS DATE)
-                ELSE T1.load_dt - CAST(T1.disability_dt AS DATE) END,0) AS "Days Aging"
+                ELSE T5.short_dt - CAST(T1.disability_dt AS DATE) END,0) AS "Days Aging"
 	, FLOOR("Days Aging"/30) AS "Months Aging"
 	, T6.manager_last_nm || ', ' || T6.manager_first_nm AS "Manager"
 	, COALESCE((T6.employee_last_nm || ', ' || T6.employee_first_nm),'Unknown') AS "Examiner"
@@ -80,21 +80,16 @@ LEFT JOIN (SELECT DISTINCT T1.claim_no
 			, MAX(CASE WHEN upper(T1.code) = 'PC' THEN T1.delete_date END) AS pc_del_dt
 			, MAX(CASE WHEN upper(T1.code) = 'PE' THEN T1.delete_date END) AS pe_del_dt
 			, MAX(CASE WHEN upper(T1.code) = 'PR' THEN T1.delete_date END) AS pr_del_dt
-		FROM (SELECT T1.claim_no, T1.dt, T1.code, T1.delete_date 
+		FROM (SELECT T1.claim_no, T1.dt, T1.code, T1.delete_date
 				FROM dibs.claim_status T1
-				--WHERE T1.type2_current_flag = 1
+				WHERE T1.type2_current_flag = 1
 				LIMIT 1 OVER (PARTITION BY T1.claim_no,T1.code ORDER BY T1.dt DESC,T1.delete_date DESC)) T1
 		GROUP BY 1) T2 ON T1.claim_num = T2.claim_no
 LEFT JOIN (SELECT * FROM dibs.customer
 			WHERE type2_current_flag = 1
 			LIMIT 1 OVER (PARTITION BY customer_id ORDER BY claim_no DESC)) T3 ON T1.dibs_customer_id = T3.customer_id
-LEFT JOIN dma.dma_dim_date T5 ON T5.short_dt BETWEEN T1.begin_dt AND T1.end_dt 
+LEFT JOIN dma.dma_dim_date T5 ON T5.short_dt BETWEEN T1.begin_dt AND T1.end_dt
 LEFT JOIN dma.dic_dim_medical_review T4 ON T1.short_claim_num = T4.claim_num AND T5.short_dt BETWEEN T4.begin_dt AND T4.end_dt
-LEFT JOIN dma.dma_dim_employee_pit T6 ON T1.examiner_party_employee_id = T6.party_employee_id AND T5.short_dt BETWEEN t6.begin_dt AND T6.end_dt 
+LEFT JOIN dma.dma_dim_employee_pit T6 ON T1.examiner_party_employee_id = T6.party_employee_id AND T5.short_dt BETWEEN t6.begin_dt AND T6.end_dt
 WHERE lower(T1.claim_status_category) NOT IN ('closed','unknown')
-AND T5.short_dt = coalesce(:date, CAST(CURRENT_DATE AS Date))
---AND T1.short_claim_num = '18-00030'
---AND T5.short_dt = '2022-05-11'
---AND T1.claim_status_category = 'Active Pending'
---AND T1.claim_category = 'Disability'
---AND T1.base_claim_ind = 1
+	AND T5.short_dt = coalesce(:date, CAST(CURRENT_DATE AS Date))
