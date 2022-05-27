f5b456a0a67e03291bc47820eb80c5d695481f29
MASSMUTUAL\mm58992 <javgoustakis@massmutual.com>
2022-05-27 09:49:18 -0400

    Vertica Migration

--- TABLEAU/EXTRACTS/PROD/DIC/DI Claims Closure Reasons.sql
+++ TABLEAU/EXTRACTS/PROD/DIC/DI Claims Closure Reasons.sql
@@ -1,31 +1,46 @@
-SELECT T1.ClaimNumber AS "Claim Number"
-                , T2.ClaimDimensionUniqueID
-                , T2.ShortClaimNumber AS "Short Claim Number"
-                , T2.PolicyNumber AS "Policy Number"
-                ,T2.ClaimCategory AS "Claim Category"
-                , T2.ExaminerPartyEmployeeID
-                , T4.EmployeeLastName || ', ' || T4.EmployeeFirstName AS "Examiner"
-                , T4.ManagerLastName || ', ' || T4.ManagerFirstName AS "Manager"
-                , T2.DisabilityDate
-                , T2.OpenDate  AS "Open Date"
-                , T1.ClaimStatusDate AS "Closed Date"
-                , T1.ClaimStatusCode AS "Status Code"
-                , T1.CloseRecloseReason "Close Reason Code"
-                , T1.TransDate AS "Transaction Date"
-                , T3.ClaimStatusDate AS "Last Approved Date"
-                , T2.ICD1Description AS Diagnosis
-                , T2.ICD1GroupName AS "Diagnosis Group"
-                , T5.CloseReasonName AS "Close Reason Name"
-                , T5.CloseReasonCategory AS "Close Reason Category"
-FROM PROD_DMA_VW.DBO_DIBS_CLAIM_STATUS_VW T1 
-INNER JOIN PROD_DMA_VW.DI_CLAIM_CURR_DIM_VW T2 ON T1.ClaimNumber = T2.ClaimNumber
-LEFT JOIN (SELECT T1.*
-                                                                                                FROM PROD_DMA_VW.DBO_DIBS_CLAIM_STATUS_VW T1
-                                                                                                WHERE ClaimStatusCode = 'AC' AND Type2CurrentFlag = 1 AND ClaimStatusDate >= '2016-01-01'
-                                                                                                QUALIFY ROW_NUMBER() OVER (PARTITION BY ClaimNumber ORDER BY ClaimStatusDate DESC) =1) T3 ON T1.ClaimNumber = T3.ClaimNumber
-LEFT JOIN PROD_DMA_VW.EMPLOYEE_CURR_DIM_VW T4 ON T2.ExaminerPartyEmployeeID = T4.PartyEmployeeID
-LEFT JOIN PROD_DMA_VW.DIC_CLOSE_REASON_LOV_VW T5 ON T1.CloseRecloseReason = T5.CloseReasonCode
-WHERE T1.ClaimStatusCode IN ('CL','RC')
-   				AND T2.OpenDate >= '2016-01-01' --Nothing opened prior to 1/1/2016
-   				AND T1.Type2CurrentFlag = 1
-   				AND T2.CurrentSubstatus LIKE ANY ('%AC%','%DN%')
+/*
+FILENAME: DI CLAIMS CLOSURE REASONS
+CREATED BY: John Avgoutakis
+LAST UPDATED: 5/5/2022
+CHANGES MADE: Vertica SQL Creation.
+*/
+
+SELECT
+
+	  T1.claim_no AS "Claim Number"
+	, T2.dim_claim_natural_key_hash_uuid AS "ClaimDimensionUniqueID"
+	, T2.short_claim_num AS "Short Claim Number"
+	, T2.policy_num AS "Policy Number"
+	, T2.claim_category AS "Claim Category"
+	, T2.examiner_party_employee_id AS "ExaminerPartyEmployeeID"
+	, T4.employee_last_nm ||','|| T4.employee_first_nm AS "Examiner"
+	, T4.manager_last_nm || ','|| T4.manager_first_nm AS "Manager"
+	, T2.disability_dt AS "Disability Date"
+	, T1.dt AS "Closed Date"
+	, T1.code AS "Status Code"
+	, T1.close_reclose_reason AS "Close Reason Code"
+	, T3.dt AS "Last Approved Date"
+	, T2.icd_1_desc AS "Diagnosis"
+	, T2.icd_1_group_nm AS "Diagnosis Group"
+	, T5.close_reason_nm AS "Close Reason Name"
+	, T5.close_reason_cat AS "Close Reason Category"
+	, T2.row_process_dtm AS "Transaction Date"
+	
+FROM dibs.claim_status T1
+
+INNER JOIN dma_vw.dic_dim_claim_curr_vw T2 ON T1.claim_no = T2.claim_num 
+
+LEFT JOIN 
+		(SELECT *
+		 FROM dibs.claim_status T1
+		 WHERE UPPER(T1.code) = 'AC' AND T1.type2_current_flag = 1 AND T1.dt >= '2016-01-01'
+		 LIMIT 1 OVER(PARTITION BY T1.claim_no ORDER BY dt DESC)) T3 ON T1.claim_no = T3.claim_no
+
+LEFT JOIN dma_vw.dma_dim_employee_curr_vw T4 ON T2.examiner_party_employee_id = T4.party_employee_id 
+
+LEFT JOIN dma_vw.dic_ref_close_reason_vw T5 ON T1.close_reclose_reason = T5.close_reason_cd
+
+WHERE UPPER(T1.code) IN ('CL','RC')
+AND T2.open_dt >= '2016-01-01'  --Nothing opened prior to 1/1/2016
+AND T1.type2_current_flag = 1
+AND (UPPER(T2.current_substatus) like '%AC%' OR UPPER(T2.current_substatus) like '%DN%')
