8ca16a0014dcf6d82cb78dc258dde5400ee6e5a9
JJohnson-MM <38733231+JJohnson-MM@users.noreply.github.com>
2022-05-27 14:50:22 -0400

    Update LA Claims Actual and Forecast.sql

--- TABLEAU/EXTRACTS/PROD/Embedded Data Sources/LA Claims Actual and Forecast.sql
+++ TABLEAU/EXTRACTS/PROD/Embedded Data Sources/LA Claims Actual and Forecast.sql
@@ -16,10 +16,10 @@ WHEN FunctionName LIKE 'Life Pay%' THEN 'Life Pay'
 WHEN FunctionName = 'Operations Setup' AND SegmentName = 'Life Setup' THEN 'Operations Setup'
 WHEN FunctionName LIKE 'Life Calc%' THEN 'Life Calc and Quotes'
 END AS "Workrole"
-,TRUNC(ReceivedDate,'MON') AS "Date"
+,Trunc(ReceivedDate,'MON') AS "Date"
 ,EmployeeRoleName
-,COALESCE(EmployeeLAStName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"   
-,COALESCE(ManagerlAStName || ', ' || ManagerFirstName, 'Unkonwn') AS "Manager"
+,Coalesce(EmployeeLAStName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"   
+,Coalesce(ManagerlAStName || ', ' || ManagerFirstName, 'Unkonwn') AS "Manager"
 ,TeamName
 ,FunctionName
 ,CASE
@@ -47,22 +47,22 @@ END AS "WorkFunction"
 ,DivisionCode
 ,TAT
 ,ShortComment
-,CAST (NULL AS SMALLINT) AS ComplexityLevel
-,MAX(TransDate) AS "MaxTransDate"
-,COUNT(DISTINCT IntegratedActivityID)  AS "Volume"
+,Cast (NULL AS SMALLINT) AS ComplexityLevel
+,Max(TransDate) AS "MaxTransDate"
+,Count(DISTINCT IntegratedActivityID)  AS "Volume"
 ,CASE 
-WHEN "WorkFunction" = 'Operations 1st Notice' THEN SUM(28.50/60.00)
-WHEN "WorkFunction" = 'Life Holds' THEN SUM(17.00/60.00)
-ELSE SUM(CurrentProdCredit)/(60.00)
+WHEN "WorkFunction" = 'Operations 1st Notice' THEN Sum(28.50/60.00)
+WHEN "WorkFunction" = 'Life Holds' THEN Sum(17.00/60.00)
+ELSE Sum(CurrentProdCredit)/(60.00)
 END AS "Demand"
-,CAST (NULL AS INTEGER) AS ForecastVolume_high95
-,CAST (NULL AS INTEGER) AS ForecastVolume_high80
-,CAST (NULL AS INTEGER) AS ForecastVolume_low80
-,CAST (NULL AS INTEGER) AS ForecastVolume_low95
-,CAST (NULL AS INTEGER) AS ForecastDemand_high95
-,CAST (NULL AS INTEGER) AS ForecastDemand_high80
-,CAST (NULL AS INTEGER) AS ForecastDemand_low80
-,CAST (NULL AS INTEGER) AS ForecastDemand_low95
+,Cast (NULL AS INTEGER) AS ForecastVolume_high95
+,Cast (NULL AS INTEGER) AS ForecastVolume_high80
+,Cast (NULL AS INTEGER) AS ForecastVolume_low80
+,Cast (NULL AS INTEGER) AS ForecastVolume_low95
+,Cast (NULL AS INTEGER) AS ForecastDemand_high95
+,Cast (NULL AS INTEGER) AS ForecastDemand_high80
+,Cast (NULL AS INTEGER) AS ForecastDemand_low80
+,Cast (NULL AS INTEGER) AS ForecastDemand_low95
 FROM PROD_DMA_VW.ACT_LAC_PIT_INTEGRATED_VW T1
 WHERE  (WorkEventDepartmentID IN (8)
 OR DepartmentID IN (8))
@@ -70,48 +70,48 @@ AND TransactionTypeID = 1
 AND SequenceNumber = 1
 AND AdminSystem <> 'PALLM'
 AND EmployeeRoleName IS NOT NULL
-AND EXTRACT(YEAR FROM ReceivedDate) >= EXTRACT(YEAR FROM CURRENT_DATE)-5
+AND Extract(YEAR From ReceivedDate) >= Extract(YEAR From Current_Date)-5
 AND "WorkRole" IN ('Life Claim Examiner', 'Operations Setup', 'Life Pay', 'Life Calc and Quotes')
 GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34
 
 UNION ALL
 
 SELECT 
-CAST ('Forecast' AS VARCHAR (50)) AS "TransactionTypeName"
+Cast ('Forecast' AS VARCHAR (50)) AS "TransactionTypeName"
 ,ForecastID
 ,NULL AS "SourceTransactionID" 
-,CAST (NULL AS  VARCHAR (40)) AS "HoldingKey" 
+,Cast (NULL AS  VARCHAR (40)) AS "HoldingKey" 
 ,WorkRole
 ,T1.ForecastDate AS "Date"
-,CAST (NULL AS VARCHAR (50)) AS "EmployeeRoleName"
-,CAST (NULL AS VARCHAR (100))  AS "Employee" 
-,CAST (NULL AS VARCHAR (100)) AS "Manager"
-,CAST (NULL AS VARCHAR (100)) AS "TeamName"
-,CAST(NULL  AS VARCHAR(50)) AS "FunctionName"  
+,Cast (NULL AS VARCHAR (50)) AS "EmployeeRoleName"
+,Cast (NULL AS VARCHAR (100))  AS "Employee" 
+,Cast (NULL AS VARCHAR (100)) AS "Manager"
+,Cast (NULL AS VARCHAR (100)) AS "TeamName"
+,Cast(NULL  AS VARCHAR(50)) AS "FunctionName"  
 ,WorkFunction
-,CAST (NULL AS VARCHAR (100)) AS "SegmentName"  
-,CAST ('Forecast' AS VARCHAR (100)) AS "WorkEventName"   
-,CAST (NULL AS VARCHAR (100)) AS "Priority"       
-,CAST (NULL AS VARCHAR (100)) AS "AdminSystem"     
-,CAST (NULL AS VARCHAR (100)) AS "ProcessName"   
-,CAST(NULL AS INTEGER ) AS "ProcessID"  
-,CAST(NULL AS INTEGER ) AS "ProcessOrder" 
-,CAST(NULL AS VARCHAR (100)) AS "ServiceChannelName"   
-,CAST(NULL AS VARCHAR (100)) AS "PartyTypeName"  
-,CAST(NULL AS VARCHAR (100)) AS "EmployeeOrganizationName"  
-,CAST(NULL AS VARCHAR (100)) AS "EmployeeDepartmentName" 
-,CAST(NULL AS VARCHAR (100)) AS "SiteName"  
-,CAST(NULL AS VARCHAR (100)) AS "WorkEventOranizationName"  
-,CAST(NULL AS VARCHAR (100)) AS "WorkEventDepartmentName"  
-,CAST(NULL AS VARCHAR (100)) AS "PrimaryRoleName"  
-,CAST(NULL AS VARCHAR (100)) AS  "SystemName" 
-,CAST(NULL AS INTEGER) AS  "WorkEventNumber" 
-,CAST (NULL AS VARCHAR (2)) AS  "DepartmentCode" 
-,CAST (NULL AS VARCHAR (2)) AS "DivisionCode" 
-,CAST (NULL AS INTEGER) AS "TAT"
-,CAST (NULL AS VARCHAR (50)) AS  "ShortComment" 
+,Cast (NULL AS VARCHAR (100)) AS "SegmentName"  
+,Cast ('Forecast' AS VARCHAR (100)) AS "WorkEventName"   
+,Cast (NULL AS VARCHAR (100)) AS "Priority"       
+,Cast (NULL AS VARCHAR (100)) AS "AdminSystem"     
+,Cast (NULL AS VARCHAR (100)) AS "ProcessName"   
+,Cast(NULL AS INTEGER ) AS "ProcessID"  
+,Cast(NULL AS INTEGER ) AS "ProcessOrder" 
+,Cast(NULL AS VARCHAR (100)) AS "ServiceChannelName"   
+,Cast(NULL AS VARCHAR (100)) AS "PartyTypeName"  
+,Cast(NULL AS VARCHAR (100)) AS "EmployeeOrganizationName"  
+,Cast(NULL AS VARCHAR (100)) AS "EmployeeDepartmentName" 
+,Cast(NULL AS VARCHAR (100)) AS "SiteName"  
+,Cast(NULL AS VARCHAR (100)) AS "WorkEventOranizationName"  
+,Cast(NULL AS VARCHAR (100)) AS "WorkEventDepartmentName"  
+,Cast(NULL AS VARCHAR (100)) AS "PrimaryRoleName"  
+,Cast(NULL AS VARCHAR (100)) AS  "SystemName" 
+,Cast(NULL AS INTEGER) AS  "WorkEventNumber" 
+,Cast (NULL AS VARCHAR (2)) AS  "DepartmentCode" 
+,Cast (NULL AS VARCHAR (2)) AS "DivisionCode" 
+,Cast (NULL AS INTEGER) AS "TAT"
+,Cast (NULL AS VARCHAR (50)) AS  "ShortComment" 
 ,ComplexityLevel
-,CAST (NULL AS DATE) AS "MaxTransDate"
+,Cast (NULL AS DATE) AS "MaxTransDate"
 , ForecastVolume AS "Volume"
 , ForecastDemand AS "Demand"
 , ForecastVolume_high95
@@ -125,64 +125,64 @@ CAST ('Forecast' AS VARCHAR (50)) AS "TransactionTypeName"
 FROM DMA_GRP_DL.Analytics_DemandFx AS T1
 INNER JOIN
 
-(SELECT DISTINCT Department, ForecastDate ,MAX(ForecastID) OVER (PARTITION BY Department,ForecastDate) AS FxID 
+(SELECT DISTINCT Department, ForecastDate ,Max(ForecastID) Over (PARTITION BY Department,ForecastDate) AS FxID 
 FROM DMA_GRP_DL.Analytics_DemandFx
-WHERE   EXTRACT(YEAR FROM ForecastDate) = EXTRACT (YEAR FROM CURRENT_DATE- INTERVAL '0' YEAR)
+WHERE   Extract(YEAR From ForecastDate) = Extract (YEAR From Current_Date- INTERVAL '0' YEAR)
 AND Department = 'Life Claims') AS T2
 ON (T1.ForecastID = FxID AND T1.ForecastDate = T2.ForecastDate AND T1.Department=T2.Department)
 
 UNION ALL
 
 SELECT
-CAST ('Received' AS VARCHAR (50)) AS "TransactionTypeName"
+Cast ('Received' AS VARCHAR (50)) AS "TransactionTypeName"
 ,NULL AS "ForecastID"
 ,NULL AS "SourceTransactionID" 
-,CAST (NULL AS  VARCHAR (40)) AS "HoldingKey" 
+,Cast (NULL AS  VARCHAR (40)) AS "HoldingKey" 
 ,RoleName AS "WorkRole"
-,TRUNC(MeetingDate,'MON') AS "Date"
+,Trunc(MeetingDate,'MON') AS "Date"
 ,RoleName AS "EmployeeRoleName"
-,COALESCE(EmployeeLAStName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"   
-,COALESCE(ManagerlAStName || ', ' || ManagerFirstName, 'Unkonwn') AS "Manager"
+,Coalesce(EmployeeLAStName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"   
+,Coalesce(ManagerlAStName || ', ' || ManagerFirstName, 'Unkonwn') AS "Manager"
 ,TeamName
-,CAST(NULL  AS VARCHAR(50)) AS "FunctionName"  
+,Cast(NULL  AS VARCHAR(50)) AS "FunctionName"  
 ,'Non-Recorded Production' AS "WorkFunction"
-,CAST (NULL AS VARCHAR (100)) AS "SegmentName"  
-,CAST (NULL AS VARCHAR (100)) AS "WorkEventName"   
-,CAST (NULL AS VARCHAR (100)) AS "Priority"       
-,CAST (NULL AS VARCHAR (100)) AS "AdminSystem"     
-,CAST (NULL AS VARCHAR (100)) AS "ProcessName"   
-,CAST(NULL AS INTEGER ) AS "ProcessID"  
-,CAST(NULL AS INTEGER ) AS "ProcessOrder" 
-,CAST(NULL AS VARCHAR (100)) AS "ServiceChannelName"   
-,CAST('EMPLOYEE' AS VARCHAR (100)) AS "PartyTypeName"  
+,Cast (NULL AS VARCHAR (100)) AS "SegmentName"  
+,Cast (NULL AS VARCHAR (100)) AS "WorkEventName"   
+,Cast (NULL AS VARCHAR (100)) AS "Priority"       
+,Cast (NULL AS VARCHAR (100)) AS "AdminSystem"     
+,Cast (NULL AS VARCHAR (100)) AS "ProcessName"   
+,Cast(NULL AS INTEGER ) AS "ProcessID"  
+,Cast(NULL AS INTEGER ) AS "ProcessOrder" 
+,Cast(NULL AS VARCHAR (100)) AS "ServiceChannelName"   
+,Cast('EMPLOYEE' AS VARCHAR (100)) AS "PartyTypeName"  
 ,OrganizationName AS "EmployeeOrganizationName"  
 ,DepartmentName AS "EmployeeDepartmentName" 
-,CAST(NULL AS VARCHAR (100)) AS "SiteName"  
-,CAST(NULL AS VARCHAR (100)) AS "WorkEventOranizationName"  
-,CAST(NULL AS VARCHAR (100)) AS "WorkEventDepartmentName"  
-,CAST(NULL AS VARCHAR (100)) AS "PrimaryRoleName"  
-,CAST(NULL AS VARCHAR (100)) AS  "SystemName" 
-,CAST(NULL AS INTEGER) AS  "WorkEventNumber" 
-,CAST (NULL AS VARCHAR (2)) AS  "DepartmentCode" 
-,CAST (NULL AS VARCHAR (2)) AS "DivisionCode" 
-,CAST (NULL AS INTEGER) AS "TAT"
-,CAST (NULL AS VARCHAR (50)) AS  "ShortComment" 
-,CAST (NULL AS SMALLINT) AS ComplexityLevel
-,CAST (NULL AS DATE) AS "MaxTransDate"
-,CAST (NULL AS INTEGER) AS "Volume"
+,Cast(NULL AS VARCHAR (100)) AS "SiteName"  
+,Cast(NULL AS VARCHAR (100)) AS "WorkEventOranizationName"  
+,Cast(NULL AS VARCHAR (100)) AS "WorkEventDepartmentName"  
+,Cast(NULL AS VARCHAR (100)) AS "PrimaryRoleName"  
+,Cast(NULL AS VARCHAR (100)) AS  "SystemName" 
+,Cast(NULL AS INTEGER) AS  "WorkEventNumber" 
+,Cast (NULL AS VARCHAR (2)) AS  "DepartmentCode" 
+,Cast (NULL AS VARCHAR (2)) AS "DivisionCode" 
+,Cast (NULL AS INTEGER) AS "TAT"
+,Cast (NULL AS VARCHAR (50)) AS  "ShortComment" 
+,Cast (NULL AS SMALLINT) AS ComplexityLevel
+,Cast (NULL AS DATE) AS "MaxTransDate"
+,Cast (NULL AS INTEGER) AS "Volume"
 ,Duration AS "Demand"
-,CAST (NULL AS INTEGER) AS ForecastVolume_high95
-,CAST (NULL AS INTEGER) AS ForecastVolume_high80
-,CAST (NULL AS INTEGER) AS ForecastVolume_low80
-,CAST (NULL AS INTEGER) AS ForecastVolume_low95
-,CAST (NULL AS INTEGER) AS ForecastDemand_high95
-,CAST (NULL AS INTEGER) AS ForecastDemand_high80
-,CAST (NULL AS INTEGER) AS ForecastDemand_low80
-,CAST (NULL AS INTEGER) AS ForecastDemand_low95
+,Cast (NULL AS INTEGER) AS ForecastVolume_high95
+,Cast (NULL AS INTEGER) AS ForecastVolume_high80
+,Cast (NULL AS INTEGER) AS ForecastVolume_low80
+,Cast (NULL AS INTEGER) AS ForecastVolume_low95
+,Cast (NULL AS INTEGER) AS ForecastDemand_high95
+,Cast (NULL AS INTEGER) AS ForecastDemand_high80
+,Cast (NULL AS INTEGER) AS ForecastDemand_low80
+,Cast (NULL AS INTEGER) AS ForecastDemand_low95
 FROM (
 SELECT
  RoleName
-,TRUNC(MeetingDate,'MON') AS "MeetingDate"
+,Trunc(MeetingDate,'MON') AS "MeetingDate"
 ,EmployeeFirstName
 ,EmployeeLastName
 ,ManagerFirstName
@@ -207,5 +207,5 @@ ON B.ShortDate=A.MeetingDate
 WHERE DepartmentID = 8
 AND TimeType = 'Production'
 AND ParentTimeCategory = 'Non-Recorded Production'
-AND EXTRACT(YEAR FROM MeetingDate) >= EXTRACT(YEAR FROM CURRENT_DATE)-5
+AND Extract(YEAR From MeetingDate) >= Extract(YEAR From Current_Date)-5
 AND RoleName IN ('Life Claim Examiner', 'Operations Setup', 'Life Pay', 'Life Calc and Quotes')) C
