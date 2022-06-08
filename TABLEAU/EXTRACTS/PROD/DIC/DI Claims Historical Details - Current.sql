bc408f954a04fb85b355d0f79d4dca42c410e91d
MASSMUTUAL\mm58992 <javgoustakis@massmutual.com>
2022-06-08 15:48:07 -0400

    Vertica Migration

--- TABLEAU/EXTRACTS/PROD/DIC/DI Claims Historical Details - Current.sql
+++ TABLEAU/EXTRACTS/PROD/DIC/DI Claims Historical Details - Current.sql
@@ -1,79 +1,73 @@
---DI Claims Historical Details - Current
+/*
+FILENAME: DI CLAIMS HISTORICAL DETAILS CURRENT
+CREATED BY: John Avgoutakis
+LAST UPDATED: 6/3/2022
+CHANGES MADE: Pointed to Vertica and added item_count.
+*/
+
 
 SELECT
-TransactionTypeName AS "Transaction Type"  
-,TransDate AS "Transaction Date"
-,SourceTransactionID AS "Source Transaction ID" 
-,ShortClaimNumber AS "Short Claim Number" 
-,BaseClaimNumber AS "Base Claim Number"
-,ClaimNumber AS "Claim Number" 
-,CASE 
-     WHEN TRANSACTIONTYPEID = 1 THEN ReceivedDate
-     WHEN TRANSACTIONTYPEID = 2 THEN LoadDate
-     WHEN TRANSACTIONTYPEID = 3 THEN CompletedDate
-     WHEN TRANSACTIONTYPEID = 4 THEN LoggedDate
-END AS "Date"    
-,EmployeeRoleName AS "Employee Role Name"  
-,COALESCE(EmployeeLastName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"      
-,COALESCE(EmployeeManagerlastName || ', ' || EmployeeManagerFirstName, 'Unknown') AS "Manager"  
-,EmployeeTeamName AS "Team Name"  
-,WorkEventFunctionName AS "Function Name"  
-,WorkEventSegmentName AS "Segment Name"  
-,WorkEventWorkEventName AS "Work Event Name"  
-,SystemName AS "System Name"  
-,EmployeeOrganizationName AS "Employee Organization Name"  
-,EmployeeDepartmentName AS "Employee Department Name"        
-,WorkEventOrganizationName AS "Work Event Organization Name"  
-,WorkEventDepartmentName AS "Work Event Department Name"  
-,WorkEventPrimaryRoleName AS "Primary Role Name"  
-,WorkEventNumber AS "Work Event Number"  
-,WorkEventDepartmentCode AS "Department Code"  
-,WorkEventDivisionCode AS "Division Code"  
-,LoggedByTeamPartyID AS "Logged By Team Party ID"
-,LoggedByPartyEmployeeID AS "Logged By Party Employee ID"
-,CASE
-    WHEN TRANSACTIONTYPEID = 2 THEN COALESCE(DemandCredit,ProductivityCredit)
-    ELSE ProductivityCredit
-END AS "Productivity Credits"
---,COALESCE(DemandCredit, ProductivityCredit) AS "Productivity Credits"
-,TAT  
-,WorkEventDepartmentID
-,1 AS "Transaction Count"  
-,TAT AS "Total TAT Days"  
-,CASE WHEN TRANSACTIONTYPEID = 3 THEN 
-CASE WHEN WorkEventMetExpectedIndicator = 1 AND MetExpected = 1 THEN 1 ELSE 0 END   
-WHEN TRANSACTIONTYPEID = 2 THEN 
-CASE WHEN WorkEventMetExpectedIndicator = 1 AND DaysPastTAT<=0 THEN 1 ELSE 0 END   
-END AS "Met Expected Count"
-,WorkEventMetExpectedIndicator AS "Met Expected Ind Count"  
----,WorkEventCurrentProdCredit AS "Event Productivity Credits"  
-,CASE WHEN DaysPastTAT <= 0 THEN 1 ELSE 0 END AS "Met TAT Count"  
-,CASE WHEN DaysPastTAT = 1 THEN 1 ELSE 0 END AS "Past TAT 1"  
-,CASE WHEN DaysPastTAT = 2 THEN 1 ELSE 0 END AS "Past TAT 2"  
-,CASE WHEN DaysPastTAT = 3 THEN 1 ELSE 0 END AS "Past TAT 3"  
-,CASE WHEN DaysPastTAT >= 4 THEN 1 ELSE 0 END AS "Past TAT 4+"  
-,CASE WHEN SystemName = 'CATS'  THEN 'CATS' 
-WHEN WorkEventSystemDepartmentName = 'Diary'  THEN 'DIBS Diary' 
-WHEN WorkEventSystemDepartmentName = 'Payment'   THEN 'DIBS Payment' 
-WHEN  WorkEventSystemDepartmentName = 'TREX CONTENT CLOSURE LETTER'   THEN 'TREX Closure Letter' 
-WHEN  WorkEventSystemDepartmentName = 'TREX CONTENT STATUS LETTER'   THEN 'TREX Status Letter' 
-WHEN  WorkEventSystemDepartmentName = 'TREX CONTENT'   THEN 'TREX Content'
-WHEN  WorkEventSystemDepartmentName = 'TREX MAIL'   THEN 'TREX Mail'
-WHEN  WorkEventSystemDepartmentName = 'TREX WORK'   THEN 'TREX Work'
-WHEN SystemName = 'MEDVOC' THEN 'MEDVOC' 
-ELSE 'UNKNOWN' END AS "Processing System"
-,COALESCE(LogEmployeeLastName || ', ' || LogEmployeeFirstName, 'Unknown') AS "Logged by Employee" 
-,MedicalReviewRNL AS "Medical Review"
-,CAST(MedicalReviewSupportDate AS DATE) "Medical Review Support Date"
-,ReceivedDate AS "Received Date"   
-,CompletedDate AS "Completed Date"
-,PaymentCheckDate AS "Payment Check Date"
-,DIBSClaimantName AS "Claimant Name"
-,PriorityName AS "Priority Name" 
---,Rank() Over (Partition by SourcetransactionID, "DATE"ORDER By TransactionTypeID Desc) as Row Rank  -- Use this to Test EOD Pending Indicator if needed
-,CASE WHEN RANK() OVER (PARTITION BY SOURCETRANSACTIONID,"DATE"  ORDER BY TRANSACTIONTYPEID DESC) = 1
-    AND TRANSACTIONTYPEID = 2 THEN 1 ELSE 0 END AS  "EOD Pending Indicator"
-
-FROM PROD_DMA_VW.ACT_DIC_CURR_INTEGRATED_VW
-WHERE (RestrictedClaimIndicator = 0 OR RestrictedClaimIndicator IS NULL)
-AND (WorkEventDepartmentID = 6 OR EmployeeDepartmentID = 6)
+	  T1.transaction_type_nm AS "Transaction Type"
+	, T1.row_process_dtm AS "Transaction Date"
+	, T1.source_transaction_id AS "Source Transaction ID"
+	, T1.fact_integrated_natural_key_hash_uuid AS "Natural Key"
+	, T1.short_claim_num AS "Short Claim Number"
+	, base_claim_num AS "Base Claim Number"
+	, T1.claim_num AS "Claim Number"
+	, T1.report_dt AS "Date"
+	, T1.employee_role_nm AS "Employee Role Name"
+	, COALESCE(employee_last_nm || ', ' || employee_first_nm, 'Unknown') AS 'Employee'
+	, COALESCE(manager_last_nm || ', ' || manager_first_nm , 'Unknown') AS 'Manager'
+	, T1.employee_team_nm AS "Team Name"
+	, T1.work_event_function_nm AS "Function Name"
+	, T1.work_event_segment_nm AS "Segment Name"
+	, T1.work_event_nm AS "Work Event Name"	
+	, T1.work_event_system_nm AS "System Name"	
+	, T1.employee_organization_nm AS "Employee Organization Name"
+	, T1.employee_department_nm AS "Employee Department Name"	
+	, T1.work_event_organization_nm AS "Work Event Organization Name"
+	, T1.work_event_department_nm AS "Work Event Department Name"	
+	, T1.work_event_primary_role_nm AS "Primary Role Name"
+	, T1.work_event_num AS "Work Event Number"
+	, T1.department_cd AS "Department Code"
+	, T1.division_cd AS "Division Code"		
+	, T1.logged_by_team_party_id AS "Logged By Team Party ID"
+	, T1.logged_by_party_employee_id AS "Logged By Party Employee ID"
+	, CASE WHEN T1.trans_type_id = 2 THEN COALESCE(T1.demand_credit,T1.current_prod_credit) ELSE T1.current_prod_credit END AS "Productivity Credits"	
+	, T1.tat AS "TAT" 	
+	, T1.work_event_department_id AS WorkEventDepartmentID
+	, 1 AS "Transaction Count"	
+	, T1.tat AS "Total TAT Days"	
+	,CASE WHEN T1.trans_type_id = 3 THEN 
+	  	  CASE WHEN T1.work_event_met_expected_ind = 1 AND T1.met_expected = 1 THEN 1 ELSE 0 END   
+		   	   WHEN T1.trans_type_id = 2 THEN 
+	  	  CASE WHEN T1.work_event_met_expected_ind = 1 AND T1.days_past_tat <= 0 THEN 1 ELSE 0 END   
+	END AS "Met Expected Count"	
+	, T1.work_event_met_expected_ind AS "Met Expected Ind Count"
+	, CASE WHEN T1.days_past_tat <= 0 THEN 1 ELSE 0 END AS "Met TAT Count"
+	, CASE WHEN T1.days_past_tat = 1 THEN 1 ELSE 0 END AS "Past TAT 1"
+	, CASE WHEN T1.days_past_tat = 2 THEN 1 ELSE 0 END AS "Past TAT 2"
+	, CASE WHEN T1.days_past_tat = 3 THEN 1 ELSE 0 END AS "Past TAT 3"
+	, CASE WHEN T1.days_past_tat >= 4 THEN 1 ELSE 0 END AS "Past TAT 4+"	
+	, CASE WHEN upper(T1.work_event_system_nm) = 'CATS'THEN 'CATS'
+		   WHEN upper(T1.system_department_nm) = 'DIARY'THEN 'DIBS Diary'
+		   WHEN upper(T1.system_department_nm) = 'PAYMENT'THEN 'DIBS Payment'
+		   WHEN upper(T1.system_department_nm) = 'TREX CONTENT CLOSURE LETTER'THEN 'TREX Closure Letter'
+		   WHEN upper(T1.system_department_nm) = 'TREX CONTENT STATUS LETTER'THEN 'TREX Status Letter'
+		   WHEN upper(T1.system_department_nm) = 'TREX CONTENT'THEN 'TREX Content'
+		   WHEN upper(T1.system_department_nm) = 'TREX MAIL'THEN 'TREX Mail'
+		   WHEN upper(T1.system_department_nm) = 'TREX WORK'THEN 'TREX Work'
+		   WHEN upper(T1.work_event_system_nm) = 'MEDVOC'THEN 'MEDVOC'	
+	  ELSE 'UNKNOWN' END AS "Processing System"		
+,COALESCE(T1.log_employee_last_nm || ', ' || T1.log_employee_first_nm, 'Unknown') AS "Logged by Employee" 
+	, T1.medical_review_rnl AS "Medical Review"
+	, CAST(T1.medical_review_support_dt AS DATE) AS "Medical Review Support Date"
+	, T1.received_dt AS "Received Date"
+	, T1.completed_dt AS "Completed Date"
+	, check_dt AS "Payment Check Date"
+	, T1.dibs_customer_nm AS "Claimant Name"
+	, T1.priority_nm AS "Priority Name"	
+	--No Longer Needed EOD Pending Indicator
+FROM dma_vw.fact_integrated_dic_curr_vw T1
+WHERE (restricted_row_ind = 0 OR restricted_row_ind IS NULL)
+AND (work_event_department_id = 6 OR employee_department_id  = 6)
