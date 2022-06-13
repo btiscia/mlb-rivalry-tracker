14a32116ab662961121589723bd53c86c2f8d25f
MASSMUTUAL\mm58992 <javgoustakis@massmutual.com>
2022-06-13 14:15:50 -0400

    Vertica Migration

--- TABLEAU/EXTRACTS/PROD/ANO/Ann Ops Historical Details - Point in Time.sql
+++ TABLEAU/EXTRACTS/PROD/ANO/Ann Ops Historical Details - Point in Time.sql
@@ -1,98 +1,79 @@
-/* This routine provides Historical Point In Time  for Annuity Operations
-
-*  Peer Review & Change Log:  
-*  Peer Review Date: 
-*  Source for this routine is  PROD_DMA_VW.ANO_ADO_PIT_INTEGRATED_FCT_VW
-*  Author: Christina Valenti, Lorraine Christian, Kristin Carlile
-*  Created: NA
-*  Revised:  7/8/2019
-*  Revision Made:  7/8/2019 - Pending removed entirely .  GoalValue field removed along with the left join to the goal table, CompletedLongDate added.  
- * Goals added back in 4/16/2020
- * 7/14/2020 Added an addition NIGO Code of '361'.  This is applicable for Annuity Only.  Refer to IGO Count.  - LC
-====================================================================== */
+/*
+FILENAME: ANNUITY OPERATIONS HISTORICAL DETAILS POINT IN TIME
+CREATED BY: John Avgoustakis, William Trombley
+LAST UPDATED: 06/10/2022
+CHANGES MADE: Vertica Migration
+*/
 
 
-SELECT 
-TransactionTypeName AS "Transaction Type"
-,SourceTransactionID AS "Source Transaction ID"
-,HoldingKey AS "Policy Number"
+SELECT
+	  T1.transaction_type_nm AS "Transaction Type"
+	, T1.fact_integrated_natural_key_hash_uuid AS "Natural Key"
+	, T1.source_transaction_id AS "Source Transaction ID"
+	, T1.agreement_nr AS "Policy Number"
+	, T1.report_dt AS "Date"
+	, T1.employee_role_nm AS "Employee Role Name"
+	, COALESCE(employee_last_nm || ', ' || employee_first_nm, 'Unknown') AS 'Employee'
+	, COALESCE(manager_last_nm || ', ' || manager_first_nm , 'Unknown') AS 'Manager'
+	, T1.employee_team_nm AS "Team Name"
+	, T1.function_nm AS "Function Name"
+	, T1.segment_nm AS "Segment Name"
+	, T1.work_event_nm AS "Work Event Name"
+	, T1.priority_id AS "Priority"
+	, T1.admn_sys_cde AS "Admin System"
+	, T1.admn_sys_id AS "Admin System ID"
+	, T1.process_nm AS "Process Name"
+	, T1.process_id AS "Process ID"
+	, T1.process_order AS "Process Order"
+	, T1.chnl_dspy_nm AS "Service Channel Code" 
+	, T1.party_type_nm AS "Party Type Name"
+	, T1.employee_organization_nm AS "Employee Organization Name"
+	, T1.employee_department_nm AS "Employee Department Name"
+	--, T1.mmid AS "MMID"
+	, T1.site_nm AS "Site Name"
+	, T1.work_event_organization_nm AS "Work Event Organization Name"
+	, T1.work_event_department_nm AS "Work Event Department Name"
+	, T1.primary_role_nm AS "Primary Role Name"
+	, T1.work_event_system_nm AS "System Name"
+	, T1.work_event_num AS "Work Event Number"
+	, T1.department_cd AS "Department Code"
+	, T1.division_cd AS "Division Code"	
+	, CASE WHEN T1.trans_type_id = 3 THEN 1 ELSE 0 END AS "Completed Indicator"
+	, T1.system_division_nm AS "Line of Business"
+	, T1.tat AS "TAT" 
+	, T1.days_past_tat AS "Days Past TAT"
+	, T1.days_past_tat AS "Total TAT Days" --Should be reviewed
+	, T1.trans_type_id "TransactionTypeId"
+	, T1.long_completed_dt AS "Completed Time Stamp"
+	, T1.NIGO_des AS "NIGODescription"
+	, CASE WHEN T1.igo_ind = 1 AND T1.nigo_cd = '-99' THEN 1 ELSE 0 END AS "NIGO Count"
+	, CASE WHEN T1.igo_ind = 1 AND T1.nigo_cd IN ('090','361') THEN 1 ELSE 0 END AS "IGO Count"
+	, T1.igo_ind AS "IGO NIGO Count"
+	, T1.short_comment AS "Short Comments"
+	--, T1.rqstr_des AS "Requestor Type Name"
+	--, T1.ProductTypeName AS "Product Type Name"
+	, CASE WHEN T1.work_event_met_expected = 1 AND days_past_tat <= 0 THEN 1 ELSE 0 END AS "Met Expected Count"
+	, T1.work_event_met_expected AS "Met Expected Ind Count"
+	, T1.row_process_dtm AS "Transaction Date"
+	, CASE WHEN T1.source_system_id = 24 THEN T1.prod_credit ELSE T1.current_prod_credit END AS "Productivity Credits"
+	, T1.flex_ind AS "Flex Count"
+	, T1.bcc_ind AS "Society 1851"
+	, CASE WHEN days_past_tat <= 0 THEN 1 ELSE 0 END AS "Met TAT Count"
+	, CASE WHEN days_past_tat = 1 THEN 1 ELSE 0 END AS "Past TAT 1"
+	, CASE WHEN days_past_tat = 2 THEN 1 ELSE 0 END AS "Past TAT 2"
+	, CASE WHEN days_past_tat = 3 THEN 1 ELSE 0 END AS "Past TAT 3"
+	, CASE WHEN days_past_tat >= 4 THEN 1 ELSE 0 END AS "Past TAT 4+"
+	,  CASE WHEN T1.major_product_type_desc IS NULL AND T1.primary_log_id IN (6,10) THEN 'Unknown'
+			WHEN T1.major_product_type_desc IS NULL AND T1.primary_log_id NOT IN (6,10) THEN NULL
+			ELSE T1.major_product_type_desc
+	   END AS "Contract Type"
+	, T1.item_ct AS "Transaction Count"
+	, T2.goal_val AS "IGO GOAL"
+FROM dma_vw.fact_integrated_ano_pit_vw T1
 
-,CASE WHEN BCCIndicator = 0
-	THEN 'N'
-	ELSE 'Y'
-END AS "Society 1851"
+LEFT OUTER JOIN (SELECT goal_val, department_id, function_id FROM dma_vw.dma_dim_goal_pit_vw WHERE end_dt = '9999-12-31' AND goal_type_id = 5) T2
+ON T1.function_id = T2.function_id AND T1.employee_department_id = T2.department_id
 
-,systemDivisionname AS "Line of Business"
-        
-,CASE 
-     WHEN TRANSACTIONTYPEID = 1 THEN ReceivedDate
---     WHEN TRANSACTIONTYPEID = 2 THEN LoadDate
-     WHEN TRANSACTIONTYPEID = 3 THEN CompletedDate
-END AS "Date"    
-     
-,EmployeeRoleName AS "Employee Role Name"
-,COALESCE(EmployeeLastName || ', ' || EmployeeFirstName, 'Unknown') AS "Employee"    
-,COALESCE(ManagerlastName || ', ' || ManagerFirstName, 'Unknown') AS "Manager"
-,TeamName AS "Team Name"
-,FunctionName AS "Function Name"
-,SegmentName AS "Segment Name"
-,WorkEventName AS "Work Event Name"
-,AdminSystemID AS "Admin System ID"
-,AdminSystemCode AS "Admin System"
-,ProcessName AS "Process Name"
-,ProcessID AS "Process ID"
-,ProcessOrder AS "Process Order"
-,ServiceChannelName AS "Service Channel Code"
-,PartyTypeName AS "Party Type Name"
-,EmployeeOrganizationName AS "Employee Organization Name"
-,EmployeeDepartmentName AS "Employee Department Name"             
-,SiteName AS "Site Name"
---,WorkEventOranizationName AS "Work Event Organization Name"
---,WorkEventDepartmentName AS "Work Event Department Name"
-,PrimaryRoleName AS "Primary Role Name"
-,SystemName AS "System Name"
-,WorkEventNumber AS "Work Event Number"
-,DepartmentCode AS "Department Code"
-,DivisionCode AS "Division Code"
-,CompletedIndicator AS "Completed Indicator"
-,TAT
-,NIGODescription
-,TransDate AS "Transaction Date"
-,LongCompletedDate AS "Completed Time Stamp"
-,CATSexpectedcompleteddate AS "Follow Up Date" 
-,ShortComment AS "Short Comments"
-,ItemCount AS "Transaction Count"
-,DaysPastTAT AS "Total TAT Days"
-,CASE WHEN MetExpectedIndicator = 1 AND DaysPastTAT <= 0 THEN 1 ELSE 0 END AS "Met Expected Count"
-,MetExpectedIndicator AS "Met Expected Ind Count"
---,CurrentProdCredit AS "Productivity Credits"  Removed 4/29 and added case statement below
- , CASE
-            WHEN SrcSysID = 24 THEN ProdCredit
-            ELSE CurrentProdCredit
-            END AS "Productivity Credits"
-,CASE WHEN IGOIndicator = 1 AND NIGOCode = '-99' THEN 1 ELSE 0 END AS "NIGO Count"
-,CASE WHEN IGOIndicator = 1 AND NIGOCode IN ('090','361') THEN 1 ELSE 0 END AS "IGO Count"  -- REPLACES LINE BELOW '361' is applicable for Annuity Only. 
---,CASE WHEN IGOIndicator = 1 AND NIGOCode = '090' THEN 1 ELSE 0 END AS "IGO Count"   REMOVED THIS LINE 7/14/2020 
-,IGOIndicator AS "IGO NIGO Count"
-,GoalValue AS "IGO Goal" --added 4/16
-,FlexIndicator AS "Flex Count"
---,MajorProductName AS "Contract Type" REMOVED 4/6/2020
-, CASE 
- 		WHEN T1.MajorProductName IS NULL AND PrimaryLogID IN (6, 10) THEN 'Unknown' 
- 		WHEN T1.MajorProductName IS NULL AND PrimaryLogID NOT IN (6, 10) THEN NULL 
- 		ELSE T1.MajorProductName 
- 		END AS "Contract Type"
-,CASE WHEN DaysPastTAT <= 0 THEN 1 ELSE 0 END AS "Met TAT Count"
-,CASE WHEN DaysPastTAT = 1 THEN 1 ELSE 0 END AS "Past TAT 1"
-,CASE WHEN DaysPastTAT = 2 THEN 1 ELSE 0 END AS "Past TAT 2"
-,CASE WHEN DaysPastTAT = 3 THEN 1 ELSE 0 END AS "Past TAT 3"
-,CASE WHEN DaysPastTAT >= 4 THEN 1 ELSE 0 END AS "Past TAT 4+"
-FROM PROD_DMA_VW.ACT_ANO_PIT_INTEGRATED_VW T1
-
-LEFT OUTER JOIN (SELECT GoalValue, DepartmentID, FunctionID FROM PROD_DMA_VW.GOAL_DIM_VW WHERE EndDate = '9999-12-31' AND GoalTypeID = 5) T2 --added 4/16/20
-ON T1.FunctionID = T2.FunctionID AND T1.DepartmentID = T2.DepartmentID --added 4/16/20
-
-WHERE (WorkEventDepartmentID IN (9,11)
-OR T1. DepartmentID IN (9, 11))
-
-AND TransactionTypeId IN (1,3)
+WHERE (work_event_department_id IN (9,11) OR employee_department_id IN (9,11))
+AND T1.trans_type_id IN (1,3)
+AND CAST(T1.load_dt AS DATE)>= (Add_Months(CURRENT_DATE(), -36))
