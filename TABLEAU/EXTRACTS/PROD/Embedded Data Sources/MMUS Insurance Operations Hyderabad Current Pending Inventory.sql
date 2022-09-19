d954e5415f4ba8dccf9817c91ee4fedc22e5836a
William Trombley <wtrombley@massmutual.com>
2022-09-19 10:36:47 -0400

    Delete Duplicates

--- TABLEAU/EXTRACTS/PROD/Embedded Data Sources/MMUS Insurance Operations Hyderabad Current Pending Inventory.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-FROM PROD_DMA_VW.CURR_PEND_VW
-WHERE (EmployeeDepartmentID = 51
-OR WorkEventDepartmentID = 51)
-AND TeamName not in ('Data Management and CRM', 'Learning & Performance', 'Business Content Management & Communications')
