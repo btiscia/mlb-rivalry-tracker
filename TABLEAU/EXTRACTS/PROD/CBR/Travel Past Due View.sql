1f2aa7273feef330aa5106a2fd23a1aa21ec2dfd
Christian <lchristian@massmutual.com>
2023-04-21 08:43:19 -0400

    CBR Past Due
    Past Due Report

--- TABLEAU/EXTRACTS/PROD/CBR/Travel Past Due View.sql
+++ TABLEAU/EXTRACTS/PROD/CBR/Travel Past Due View.sql
@@ -1,2 +1,14 @@
-SELECT *
-FROM PROD_DMA_VW.TVL_PAST_DUE_RPT_VW
+/*
+* This routine pulls transactions for the past due report
+*  Peer Review & Change Log:
+*  Peer Review Date: 
+*  Source for this routine is: dma_vw.tvl_past_due_rpt_new_vw
+*  Author: Srinivas Pasumarthy
+*  Report Developer:  Lorraine Christian
+*  Created: 4/20/2023
+*  Revisions:               
+======================================================================*/
+
+SELECT * FROM
+dma_vw.tvl_past_due_rpt_new_vw
+where status <> 'FA'
