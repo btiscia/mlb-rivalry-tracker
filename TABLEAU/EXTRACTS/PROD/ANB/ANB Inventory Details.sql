316d67bc74b7df7bd2c35e94017a30ed7228fb20
jmadru-mm <jmadru@massmutual.com>
2023-05-12 11:18:20 -0400

    osdt3-5312-inv-details
    Remove product case statement as this logic is resolved in the view

--- TABLEAU/EXTRACTS/PROD/ANB/ANB Inventory Details.sql
+++ TABLEAU/EXTRACTS/PROD/ANB/ANB Inventory Details.sql
@@ -1,15 +1,14 @@
 /*
-FILENAME: ANB Inventory Details
+FILENAME: ANB Inventory Details Vertica
 UPDATED BY: Jess Madru 
-LAST UPDATED: 08/24/2022
-CHANGES MADE: Vertica Migration
+LAST UPDATED: 05/12/2023
+CHANGES MADE: Vertica Migration, remove product case statement as resolved in view
 */
-
 SELECT	DISTINCT T1.fact_inventory_natural_key_hash_uuid AS InventoryID
-	, T1.dim_agreement_natural_key_hash_uuid AS ApplicationNaturalKeyUUID
+	, T1.dim_agreement_natural_key_hash_uuid AS ApplicationNaturalKeyUUID  
 	, order_entry_id AS OrderEntryID
-	, T1.agreement_nr AS AgreementID 
-	, TRIM(LEADING '0' FROM T1.agreement_nr) AS ContractNumber
+	, T1.agreement_nr AS AgreementID  
+	, TRIM(LEADING '0' FROM T1.agreement_nr) AS ContractNumber  
 	, T2.ReportDate AS "Date"
 	,"InventoryCurrDayInd"
     , IsHoliday AS "Date Is Holiday"
@@ -17,19 +16,9 @@ SELECT	DISTINCT T1.fact_inventory_natural_key_hash_uuid AS InventoryID
 	, agent_id AS AgentID
 	, advisor_nm AS Advisor  
 	, agency_num AS AgencyNumber
-	, CASE
-   		WHEN T1.product LIKE ('%Capital Vantage%') THEN 'Capital Vantage'
-   		WHEN T1.product LIKE ('%Transition Select%') THEN 'Transition Select'
-  		WHEN T1.product LIKE ('%RetireEase Choice%') THEN 'RetireEase Choice'
-   		WHEN T1.product LIKE ('%RetireEase%') THEN 'RetireEase'
-   		WHEN T1.product LIKE ('%Stable Voyage%') THEN 'Stable Voyage'
-   		WHEN T1.product LIKE ('%Odyssey Select%') THEN 'Odyssey Select'
-   		WHEN T1.product LIKE ('%Index Horizons%') THEN 'Index Horizons'
-   		WHEN T1.product LIKE ('%Envision%') THEN 'Envision'
-   		ELSE 'Unknown'
-	  END AS Product
+	, product as "Product"
 	, product_category AS "Product Category"
-	, agency_id_prefix AS Firm
+	, agency_id_prefix AS Firm  
 	, firm_num AS "FirmID"
 	, CASE WHEN agency_num is NULL and firm_nm is NULL then '999 - Unknown' ELSE firm_nm END AS "FirmName" 
 	, anticipated_premium AS "Anticipated Premium"
@@ -59,7 +48,7 @@ SELECT	DISTINCT T1.fact_inventory_natural_key_hash_uuid AS InventoryID
     ,T2.ReportDate
     ,T2.IsHoliday
     ,T2.IsWeekday
-FROM dma_vw.sem_fact_inventory_anb_vw T1
+FROM dma_vw.sem_fact_inventory_anb_vw T1	
 LEFT JOIN   (SELECT short_dt, prev_bd, CAST(is_holiday AS INTEGER) AS "IsHoliday", CAST(is_weekday AS INTEGER) AS "IsWeekDay"
 			 ,CASE WHEN is_holiday = False AND is_weekday = True THEN short_dt ELSE prev_bd END AS ReportDate
 			 ,CASE WHEN short_dt = CURRENT_DATE THEN 1 ELSE 0 END AS "InventoryCurrDayInd"
