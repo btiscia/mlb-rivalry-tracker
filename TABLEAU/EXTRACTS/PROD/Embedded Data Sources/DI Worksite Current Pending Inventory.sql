5297fb51e79b496381b60debbee8c0945f7afd89
wtiscia <89804782+wtiscia@users.noreply.github.com>
2023-08-15 10:52:44 -0400

    Update DI Worksite CPI

--- TABLEAU/EXTRACTS/PROD/Embedded Data Sources/DI Worksite Current Pending Inventory.sql
+++ TABLEAU/EXTRACTS/PROD/Embedded Data Sources/DI Worksite Current Pending Inventory.sql
@@ -1,9 +1,9 @@
 /*
 FILENAME: DI WORKSITE CURRENT PENDING INVENTORY
-CREATED BY: BILL TISCIA 07/11/2023
-LAST UPDATED: 07/11/2023
+CREATED BY: BILL TISCIA 08/08/2023
+LAST UPDATED: 08/08/2023
 CHANGES MADE: 
-07/11/2023 - Code creation
+08/08/2023 - Code creation
 */
 
 select
@@ -28,11 +28,11 @@ t1.segment_nm,
 t1.insured_last_nm,
 t1.insured_first_nm,
 t1.insured_middle_nm,
-t1.pol_nr,
-t1.apm_grp_ident,
-t1.rcvd_dt,
-t1.expected_completed_dt,
-t1.cats_expected_completed_dt,
+trim(trim(LEADING '0' from t1.pol_nr)) as 'pol_nr',
+trim(t1.apm_grp_ident) as 'group_num',
+cast(t1.rcvd_dt as date) as 'Received Date',
+cast(t1.expected_completed_dt as date) as 'expected_completed_dt',
+cast(t1.cats_expected_completed_dt as date) as 'source_expected_completed_dt',
 t1.days_pending,
 t1.days_past_tat,
 t1.tat_goal,
@@ -48,12 +48,12 @@ null as major_prod_nm,
 case when t1.bcc_ind=0 then'N'
   when t1.bcc_ind=1 then 'Y'
    end AS 'Society 1851',
-case when t1.pol_nr is null and t1.group_nm is not null then t1.group_nm else t1.pol_nr end as 'Policy / Group #',
+case when t1.pol_nr is null and t1.apm_grp_ident  is not null then trim(t1.apm_grp_ident ) else trim(trim(LEADING '0' from t1.pol_nr)) end as 'Policy / Group #',
 t1.row_process_dtm,
 t2.system_nm as 'Data Source'
 from dma_vw.rpt_cats_curr_pend_vw t1
 left join (select distinct ref_work_event_natural_key_hash_uuid, system_nm from dma_vw.dma_dim_work_curr_vw) t2 on t1.ref_work_event_natural_key_hash_uuid = t2.ref_work_event_natural_key_hash_uuid
-where (t1.employee_department_id = 3) and (t1.function_nm <> 'Flags/Blockers' or t1.function_nm is null)
+where (t1.employee_department_id = 3 or t1.work_event_department_id = 3) and (t1.function_nm <> 'Flags/Blockers' or t1.function_nm is null)
 
 union
 
@@ -79,9 +79,9 @@ t1.segment_nm,
 t1.insured_last_nm  ,
 t1.insured_first_nm,
 t1.insured_middle_nm,
-trim(t1.pol_nr) as 'pol_nr',
+trim(trim(LEADING '0' from t1.pol_nr)) as 'pol_nr',
 trim(t1.group_num) as 'group_num',
-cast(t1.received_dt as date) as 'received_dt',
+cast(t1.received_dt as date) as 'Received Date',
 cast(t1.expected_completed_dt as date) as 'expected_completed_dt',
 cast(t1.source_expected_completed_dt as date) as 'source_expected_completed_dt',
 t1.days_pending,
@@ -99,9 +99,9 @@ t1.major_prod_nm,
 case when t1.bcc_ind is null  then'N'
   when t1.bcc_ind is not null then 'Y'
    end  as 'Society 1851',
-case when t1.pol_nr is null and t1.group_num is not null then trim(t1.group_num) else trim(t1.pol_nr) end as 'Policy / Group #',
+case when t1.pol_nr is null and t1.group_num is not null then trim(t1.group_num) else trim(trim(LEADING '0' from t1.pol_nr)) end as 'Policy / Group #',
 t1.row_process_dtm,
 t2.system_nm as 'Data Source'
 from dma_vw.dipms_curr_pend_vw t1
 left join (select distinct dim_work_natural_key_hash_uuid, system_nm from dma_vw.dma_dim_work_curr_vw) t2 on t1.dim_work_natural_key_hash_uuid = t2.dim_work_natural_key_hash_uuid
-where (t1.employee_department_id = 3) and (t1.function_nm <> 'Flags/Blockers' or t1.function_nm is null)
+where (t1.employee_department_id = 3 or t1.work_event_department_id = 3) and (t1.function_nm <> 'Flags/Blockers' or t1.function_nm is null)
