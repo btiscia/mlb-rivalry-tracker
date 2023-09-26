6c31539e210f827a980d1094207dbe491dc453d8
jmadru-mm <jmadru@massmutual.com>
2023-09-26 11:59:31 -0400

    OSDT3-5621-MMSD-ENV
    Updated ANB iPipeline Data and ANB Submit Details data sources to support isolating HO and Zinnia data in the ANB Daily New Business Submits dashboard

--- TABLEAU/EXTRACTS/PROD/ANB/ANB iPipeline Data.sql
+++ TABLEAU/EXTRACTS/PROD/ANB/ANB iPipeline Data.sql
@@ -1,15 +1,15 @@
 /*
-FILENAME: ANB iPipeline Data
-UPDATED BY: John Avgoustakis, Vince Bonaddio 
-LAST UPDATED: 06/27/2022
-CHANGES MADE: Vertica Migration
+FILENAME: ANNUITY NEW BUSINESS iPIPELINE DATA
+UPDATED BY: Jess Madru
+LAST UPDATED: 09/26/2023
+CHANGES MADE: Vertica Migration, excluded HO products
 */
 
 SELECT	
 
 	  T1.order_entry_id AS "OrderEntryID"
-    , T1.product_type AS "ProductType"
-    , T1.product_nm AS "ProductName"
+    , T1.product_category AS "ProductType"
+    , T1.product AS "ProductName"
     , T1.agency_num AS "AgencyNumber"
     , T1.agent_id AS "AgentID"
     , T1.carrier_cd AS "CarrierCode"
@@ -26,10 +26,10 @@ SELECT
     , T1.copied_from_trans_id AS "CopiedFromTransIdentifier"
     , T1.auto_approved_ind AS "AutoApprovedIndicator"
     , T1.row_process_dtm AS "TRANS_DT"
-FROM dma_vw.dim_ipipeline_orders_curr_vw T1
+FROM dma_vw.sem_anb_ipipeline_vw T1
 LEFT JOIN dma_vw.dma_dim_date_vw T2 ON T2.short_dt = CAST(T1.suit_comp_dt_transmit AS DATE)
 LEFT JOIN dma_vw.dma_dim_date_vw T3 ON T3.short_dt = CAST(T1.electronic_submit_dt AS DATE)
 WHERE T1.electronic_submit_dt IS NOT NULL   --Count for Katie's Team Work Received
  AND (carrier_cd = '65935' OR carrier_cd = '93432')
- 
+and T1.home_office_ind = 0
  
