8607a802a155ec8cf9169e78aa9a1efd19d92c87
wtiscia <89804782+wtiscia@users.noreply.github.com>
2024-03-11 11:51:32 -0400

    DIC Historical Data Sources Updates

--- TABLEAU/EXTRACTS/PROD/DIC/DI Claims Historical Details - Point in Time.sql
+++ TABLEAU/EXTRACTS/PROD/DIC/DI Claims Historical Details - Point in Time.sql
@@ -1,10 +1,12 @@
 /*
 FILENAME: DI CLAIMS HISTORICAL DETAILS - POINT IN TIME
 CREATED BY: Jess Madru
-LAST UPDATED: 1/26/2023
-CHANGES MADE: Pointed to Vertica, added Admin System field
-10/5/2023 - Added fields for Work Distribution Reporting
-CHANGES MADE:  10/20/2023 - Logged by now in mart removed join to dma_vw.dma_dim_employee_pit_vw AS T4
+LAST UPDATED: 03/05/2024
+CHANGES MADE:  
+01/26/2023 - Pointed to Vertica, added Admin System field
+10/05/2023 - Added fields for Work Distribution Reporting
+10/20/2023 - Logged by now in mart removed join to dma_vw.dma_dim_employee_pit_vw AS T4
+03/05/2024 - added channel field - Bill Tiscia
 */
 
 SELECT
@@ -13,7 +15,7 @@ SELECT
 	, T1.source_transaction_id AS "Source Transaction ID"
 	, T1.fact_integrated_natural_key_hash_uuid AS "Natural Key"
 	, T1.short_claim_num AS "Short Claim Number"
-	, base_claim_num AS "Base Claim Number"
+	, T1.base_claim_num AS "Base Claim Number"
 	, T1.claim_num AS "Claim Number"
 	, T1.admin_sys AS "Admin System"
 	, T1.report_dt AS "Date"
@@ -52,7 +54,7 @@ SELECT
 	, CASE WHEN T1.days_past_tat = 3 THEN 1 ELSE 0 END AS "Past TAT 3"
 	, CASE WHEN T1.days_past_tat >= 4 THEN 1 ELSE 0 END AS "Past TAT 4+"
 	, CASE WHEN T1.work_event_group_id = 8  THEN 1 ELSE 0 END "Referral Indicator"
-	, CASE WHEN filter_group IS NOT NULL THEN 1 ELSE 0 END "TAT Indicator"
+	, CASE WHEN T3.filter_group IS NOT NULL THEN 1 ELSE 0 END "TAT Indicator"
 	, CASE WHEN upper(T1.source_system_nm) = 'CATS' THEN 'CATS'
 		WHEN upper(T1.system_department_nm) = 'DIARY'THEN 'DIBS Diary'
 		WHEN upper(T1.system_department_nm) = 'PAYMENT'THEN 'DIBS Payment'
@@ -68,7 +70,7 @@ SELECT
 	, CAST(T1.medical_review_support_dt AS DATE) AS "Medical Review Support Date"
 	, T1.received_dt AS "Received Date"
 	, T1.completed_dt AS "Completed Date"
-	, check_dt AS "Payment Check Date"
+	, T1.check_dt AS "Payment Check Date"
 	, T1.dibs_customer_nm AS "Claimant Name"
 	, T1.priority_nm AS "Priority Name"
 	, T1.days_past_tat AS "Days Past TAT"
@@ -76,30 +78,19 @@ SELECT
 	, T1.work_event_group_nm AS "Work Event Group Name"
 	, T1.work_event_group_type_nm AS "Work Event Group Type Name"
 	, T1.role_grade_nm AS "Employee Role Grade Name"
-	, CASE    
-	when T1.role_grade_id = 14
-     then 'Bot'
-     when T1.party_type_id = 2
-     then 'System Acct'
-     when (T1.employee_department_id = 51
-        or (COALESCE(T1.employee_department_id, -99) = -99
-        and lower(T1.mmid) like 'ot%'))
-        and T1.party_type_id <> 2
-     then 'Hyderabad Employee'
-     when (T1.employee_department_id NOT IN (51, -99)
-        or (COALESCE(T1.employee_department_id, -99) = -99
-        and left(lower(T1.mmid),2) in ('mm', 'ct')))
-        and T1.party_type_id <> 2
-     then 'US Employee'
-     else 'Unknown'
-     end as "Completed By Type"
+	, CASE 
+		when T1.role_grade_id = 14 then 'Bot'
+     	when T1.party_type_id = 2  then 'System Acct'
+     	when (T1.employee_department_id = 51 or (COALESCE(T1.employee_department_id, -99) = -99 and lower(T1.mmid) like 'ot%')) and T1.party_type_id <> 2 then 'Hyderabad Employee'
+    	when (T1.employee_department_id NOT IN (51, -99) or (COALESCE(T1.employee_department_id, -99) = -99 and left(lower(T1.mmid),2) in ('mm', 'ct'))) and T1.party_type_id <> 2 then 'US Employee'
+     	else 'Unknown' 
+	 end as "Completed By Type"
      , COALESCE(T1.logged_by_employee_last_nm || ', ' || T1.logged_by_employee_first_nm, 'Unknown') AS 'Logged By'        
---   , COALESCE(T4.employee_last_nm || ', ' || T4.employee_first_nm, 'Unknown') AS 'Logged By'	
+    --, COALESCE(T4.employee_last_nm || ', ' || T4.employee_first_nm, 'Unknown') AS 'Logged By'
+    , CASE WHEN T1.mmsd_ind = True THEN 'MMSD' ELSE 'MMFA' END AS "Channel"
 FROM dma_vw.fact_integrated_dic_pit_vw T1
-/*LEFT OUTER JOIN
-dma_vw.dma_dim_employee_pit_vw AS T4
-ON T1.logged_by_team_party_id = T4.team_party_id*/
+--LEFT OUTER JOIN dma_vw.dma_dim_employee_pit_vw AS T4 ON T1.logged_by_team_party_id = T4.team_party_id
 LEFT JOIN (SELECT * FROM dma_vw.dma_rel_filter_conv_vw WHERE filter_group =30) T3 ON T1.work_event_id = T3.search_id
 WHERE (T1.restricted_row_ind = 0 OR T1.restricted_row_ind IS NULL)
-AND (work_event_department_id = 6 OR employee_department_id  = 6)
+AND (T1.work_event_department_id = 6 OR T1.employee_department_id  = 6)
 AND "Date" >= CURRENT_DATE - INTERVAL '3' YEAR
